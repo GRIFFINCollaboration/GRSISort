b1abc1229d1464c046307f82c3bab6028f38a00d
r3dunlop <r3dunlop@gmail.com>
2014-09-02 21:48:17 -0400

    Fixed html_generator to include all classes and to include GRSISYS path

--- util/html_generator.C
+++ util/html_generator.C
@@ -1,16 +1,23 @@
 #include "THtml.h"
 
 void html_generator(){
-   gSystem->Load("/opt/root/lib/libXMLIO.so");
-   gSystem->Load("/opt/root/lib/libXMLParser.so");
 
    gSystem->Load("libTreePlayer");
-   gSystem->Load("$(GRSISYS)/libraries/libMidasFormat.so");
-   gSystem->Load("$(GRSISYS)libraries/libGRSIFormat.so");
+   gSystem->Load("$GRSISYS/libraries/libMidasFormat.so");
+   gSystem->Load("$GRSISYS/libraries/libGRSIFormat.so");
+ //  gSystem->Load("$GRSISYS/libraries/libBuilder.so");
+ //  gSystem->Load("$GRSISYS/libraries/libGRSILoop.so");
+ //  gSystem->Load("$GRSISYS/libraries/libTGRSIint.so");
+   gSystem->Load("$GRSISYS/libraries/libDataParser.so");
+ //  gSystem->Load("$GRSISYS/libraries/libGRSIRootIO.so");
+   gSystem->Load("$GRSISYS/libraries/libNucleus.so");
+   gSystem->Load("$GRSISYS/libraries/libKinematics.so");
+   gSystem->Load("$GRSISYS/libraries/libSharc.so");
 
 
    THtml h;
    h.SetInputDir("$(GRSISYS)");
    h.SetOutputDir("$(GRSISYS)/htmldoc");
+   h.MakeClass("TNucleus");
    h.MakeAll();
 }
