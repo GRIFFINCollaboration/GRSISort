baeaeb998debbe848b64cbcc80085eeeaf966368
r3dunlop <r3dunlop@gmail.com>
2014-09-02 21:30:58 -0400

    Removed junk created by html

--- util/html_generator.C
+++ util/html_generator.C
@@ -1,7 +1,16 @@
 #include "THtml.h"
 
 void html_generator(){
+   gSystem->Load("/opt/root/lib/libXMLIO.so");
+   gSystem->Load("/opt/root/lib/libXMLParser.so");
+
+   gSystem->Load("libTreePlayer");
+   gSystem->Load("$(GRSISYS)/libraries/libMidasFormat.so");
+   gSystem->Load("$(GRSISYS)libraries/libGRSIFormat.so");
+
+
    THtml h;
    h.SetInputDir("$(GRSISYS)");
+   h.SetOutputDir("$(GRSISYS)");
    h.MakeAll();
 }
