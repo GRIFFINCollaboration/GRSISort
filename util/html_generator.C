1854466e806fe4157655b196cf6a11e58a7a2910
r3dunlop <r3dunlop@gmail.com>
2014-09-03 10:10:08 -0400

    Added more libraries to html

--- util/html_generator.C
+++ util/html_generator.C
@@ -2,24 +2,25 @@
 
 void html_generator(){
 
+   //Order matters!!!!
    gSystem->Load("libTreePlayer");
-/*   gSystem->Load("$GRSISYS/libraries/libMidasFormat.so");
+   gSystem->Load("$GRSISYS/libraries/libMidasFormat.so");
    gSystem->Load("$GRSISYS/libraries/libGRSIFormat.so");
  //  gSystem->Load("$GRSISYS/libraries/libBuilder.so");
- //  gSystem->Load("$GRSISYS/libraries/libGRSILoop.so");
- //  gSystem->Load("$GRSISYS/libraries/libTGRSIint.so");
-   gSystem->Load("$GRSISYS/libraries/libDataParser.so");
  //  gSystem->Load("$GRSISYS/libraries/libGRSIRootIO.so");
+   gSystem->Load("$GRSISYS/libraries/libDataParser.so");
    gSystem->Load("$GRSISYS/libraries/libNucleus.so");
    gSystem->Load("$GRSISYS/libraries/libKinematics.so");
    gSystem->Load("$GRSISYS/libraries/libSharc.so");
-*/
+   gSystem->Load("$GRSISYS/libraries/libGRSIint.so");
+   gSystem->Load("libProof");
 
    THtml html;
    html.SetProductName("GRSISort");
-   html.SetInputDir("$(GRSISYS)");
+   html.SetInputDir("$(GRSISYS)/libraries/");
+   html.SetIncludePath("$(GRSISYS)/include/");
+   html.AddMacroPath("$(GRSISYS)/util");
    html.SetOutputDir("$(GRSISYS)/htmldoc");
-   html.SetSourceDir("$(GRSISYS)/libraries/TGRSIFormat/:$(GRSISYS)/libraries/TDataParser/:$(GRSISYS)/libraries/TGint/:TGRSILoop:$(GRSISYS)/libraries/TGRSIAnalysis/:$(GRSISYS)/include/");
- //  html.MakeClass("TNucleus");
+   html.SetSourceDir("$(GRSISYS)/libraries/*/*:$(GRSISYS)/include/");
    html.MakeAll();
 }
