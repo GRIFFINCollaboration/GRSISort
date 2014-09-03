a57b8f7fa35c77f4feb775affac75f5b233cb9c6
r3dunlop <r3dunlop@gmail.com>
2014-09-03 16:18:20 -0400

    HTML works better

--- util/html_generator.C
+++ util/html_generator.C
@@ -1,5 +1,4 @@
-#include "THtml.h"
-
+/*
 void html_generator(){
 
    //Order matters!!!!
@@ -17,10 +16,127 @@ void html_generator(){
 
    THtml html;
    html.SetProductName("GRSISort");
-   html.SetInputDir("$(GRSISYS)/libraries/");
-   html.SetIncludePath("$(GRSISYS)/include/");
+   html.SetInputDir("$(GRSISYS)");
+ //  html.SetIncludePath("$(GRSISYS)/include/");
    html.AddMacroPath("$(GRSISYS)/util");
    html.SetOutputDir("$(GRSISYS)/htmldoc");
-   html.SetSourceDir("$(GRSISYS)/libraries/*/*:$(GRSISYS)/include/");
+ //  html.SetSourceDir("$(GRSISYS)/libraries/TGint:$(GRSISYS)/include/:$(GRSISYS)/src");
    html.MakeAll();
+}
+*/
+
+
+
+#include "THtml.h"
+#include <string>
+#include <sstream>
+
+class THtmlCreator: public TObject {
+public:
+   THtmlCreator(): fHtml(0)
+   { 
+      printf("This class is for demonstration purposes only!\n"); 
+   }
+   ~THtmlCreator() { if (fHtml) delete fHtml; }
+
+   // inline methods can have their documentation in front 
+   // of the declaration. DontDoMuch is so short - where
+   // else would one put it?
+   void DontDoMuch() {}
+
+   void Convert() 
+   {
+      // Create a "beautified" version of this source file.
+      // It will be called htmldoc/htmlex.C.html.
+
+      GetHtml()->SetSourceDir("$(GRSISYS)/util");
+      GetHtml()->Convert("html_generator.C", "Generates HTML Documentation", "./htmldoc/", "./");
+   }
+
+   void MakeDocForAllClasses() 
+   {
+      // Creates the documentation pages for all classes that have
+      // been loaded, and that are accessible from "./".
+      // If evenForROOT is set, we'll try to document ROOT's classes, 
+      // too - you will end up with a copy of ROOT's class reference.
+      // The documentation will end up in the subdirectory htmldoc/.
+
+      std::string inclpath = "$(GRSISYS)";
+      std::stringstream totpath;
+      totpath << inclpath << fpath.str();
+      std::string incldirs = totpath.str(); 
+      GetHtml()->SetInputDir(incldirs.c_str());
+     // GetHtml()->SetInputDir("$(GRSISYS):$(GRSISYS)/libraries/TGRSIFormat");
+    //  GetHtml()->SetIncludePath("$(GRSISYS)/include");
+     // GetHtml()->SetSourceDir("$GRSISYS/libraries");
+      GetHtml()->SetOutputDir("$GRSISYS/htmldoc");
+      GetHtml()->MakeAll(kTRUE);
+   }
+
+   void RunAll() {
+      // Do everything we can.
+      MakeDocForAllClasses();
+      Convert();
+   }
+
+   void SetProductName(const char* name)
+   {
+      //Sets Program name in HTML documentation
+      GetHtml()->SetProductName(name);
+   }
+
+   void AddSourcePath(std::string newpath)
+   {   
+       fpath << ":$(GRSISYS)/libraries/" << newpath;
+       std::cout << "THE PATH IS CURRENTLY " << fpath.str();
+   }
+
+protected:
+   THtml* GetHtml() 
+   {
+      // Return out THtml object, and create it if it doesn't exist.
+      if (!fHtml) fHtml = new THtml();
+      return fHtml;
+   }
+
+private:
+   THtml* fHtml; // our local THtml instance.
+   std::stringstream fpath;
+   ClassDef(THtmlCreator, 0); // The Html Creation Class. 
+};
+
+void html_generator() {
+
+   //Order matters!!!!
+   gSystem->Load("libTreePlayer");
+   gSystem->Load("$GRSISYS/libraries/libMidasFormat.so");
+   gSystem->Load("$GRSISYS/libraries/libGRSIFormat.so");
+ //  gSystem->Load("$GRSISYS/libraries/libBuilder.so");
+ //  gSystem->Load("$GRSISYS/libraries/libGRSIRootIO.so");
+   gSystem->Load("$GRSISYS/libraries/libDataParser.so");
+   gSystem->Load("$GRSISYS/libraries/libNucleus.so");
+   gSystem->Load("$GRSISYS/libraries/libKinematics.so");
+   gSystem->Load("$GRSISYS/libraries/libSharc.so");
+   gSystem->Load("$GRSISYS/libraries/libGRSIint.so");
+   gSystem->Load("libProof");
+
+   gSystem->ListLibraries();
+
+   THtmlCreator html;
+   html.SetProductName("GRSISort");
+
+   html.AddSourcePath("TGRSIFormat"); 
+   html.AddSourcePath("TDataParser");
+   html.AddSourcePath("TGint");
+   html.AddSourcePath("TGRSILoop");
+   html.AddSourcePath("TGRSIAnalysis");
+   html.AddSourcePath("TMidas");
+   html.AddSourcePath("TGRSIint");
+   html.AddSourcePath("TGRSIRootIO");
+   html.AddSourcePath("TGRSIAnalysis/TAnalysisTreeBuilder");
+   html.AddSourcePath("TGRSIAnalysis/TKinematics");
+   html.AddSourcePath("TGRSIAnalysis/TNucleus");
+   html.AddSourcePath("TGRSIAnalysis/TSharc");
+
+   html.RunAll();
 }
