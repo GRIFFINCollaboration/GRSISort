3a158c4c331cc89491619505e774601d2bcb3b7e
r3dunlop <r3dunlop@gmail.com>
2014-09-12 15:57:38 -0400

    Created files for TGRSIFunctions for commonly used fitting functions, etc.

---
+++ include/TGRSIFunctions.h
@@ -0,0 +1,20 @@
+#ifndef __TGRSIFUNCTIONS_H
+#define __TGRSIFUNCTIONS_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "TMath.h"
+
+namespace TGRSIFunctions {
+
+
+
+
+
+}
+
+
+
+#endif
---
+++ libraries/TGRSIAnalysis/TGRSIFunctions/TGRSIFunctions.cxx
@@ -0,0 +1,7 @@
+#include "TGRSIFunctions.h"
+
+//Without this macro the THtml doc for TGRSIFunctions can't be generated
+NamespaceImp(TGRSIFunctions);
+
+
+
