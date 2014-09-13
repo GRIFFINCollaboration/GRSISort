b75a1c9f840a9dcf4bd8d4303c23b40bd6c20954
r3dunlop <r3dunlop@gmail.com>
2014-09-12 20:59:50 -0400

    stopping point for the night

--- include/TGRSIFunctions.h
+++ include/TGRSIFunctions.h
@@ -10,9 +10,9 @@
 
 namespace TGRSIFunctions {
 
-   Double_t polybg(Double_t *, Double_t *,Int_t);
-   Double_t step_function(Double_t*, Double_t*);
-
+   Double_t PolyBg(Double_t*, Double_t*,Int_t);
+   Double_t StepFunction(Double_t*, Double_t*);
+   Double_t PhotoPeak(Double_t*, Double_t*);
 
 }
 
--- libraries/TGRSIAnalysis/TGRSIFunctions/TGRSIFunctions.cxx
+++ libraries/TGRSIAnalysis/TGRSIFunctions/TGRSIFunctions.cxx
@@ -4,7 +4,7 @@
 NamespaceImp(TGRSIFunctions);
 
 // Polynomial background function
-Double_t TGRSIFunctions::polybg(Double_t *x, Double_t *par,Int_t order) {
+Double_t TGRSIFunctions::PolyBg(Double_t *x, Double_t *par,Int_t order) {
    if(sizeof(par)/sizeof(par[0]) < (order + 2)){
       std::cout << "not enough parameters passed to function" << std::endl;
       return 0;
@@ -17,7 +17,7 @@ Double_t TGRSIFunctions::polybg(Double_t *x, Double_t *par,Int_t order) {
    return result;
 }
 
-Double_t TGRSIFunctions::step_function(Double_t *dim, Double_t *par){
+Double_t TGRSIFunctions::StepFunction(Double_t *dim, Double_t *par){
 
    Double_t x       = dim[0];
    Double_t height  = par[0];
@@ -28,4 +28,18 @@ Double_t TGRSIFunctions::step_function(Double_t *dim, Double_t *par){
    return TMath::Abs(step)*height/100.0*TMath::Erfc((x-c)/(TMath::Sqrt(2.0)*sigma));
 }
 
+Double_t TGRSIFunctions::PhotoPeak(Double_t *dim, Double_t *par){
+   
+   //Define the parameters for easy use.
+   Double_t x        = dim[0]; //channel number used for fitting 
+   Double_t height   = par[0]; //height of photopeak
+   Double_t c        = par[1]; //Peak Centroid of non skew gaus
+   Double_t sigma    = par[2]; //standard deviation  of gaussian
+   Double_t beta     = par[3]; //"skewedness" of the skewed gaussian
+   Double_t R        = par[4]; //relative height of skewed gaussian
 
+   Double_t gaussian    = height*(1.0-R/100.0)*TMath::Gaus(x,c,sigma);
+   Double_t skewed_gaus = R*height/100.0*(TMath::Exp((x-c)/beta))*(TMath::Erfc((x-c)/(TMath::Sqrt(2.0)*sigma)) + sigma/(TMath::Sqrt(2.0)*beta));
+
+   return gaussian + skewed_gaus;
+}
