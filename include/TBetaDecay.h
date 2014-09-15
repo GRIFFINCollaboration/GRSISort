d7f4c9644c709acb3f17d77d369f36c81c9e1baa
r3dunlop <r3dunlop@gmail.com>
2014-09-15 14:19:58 -0400

    TBetaDecay constructors for non-existing TNucleus

--- include/TBetaDecay.h
+++ include/TBetaDecay.h
@@ -11,14 +11,17 @@
 class TBetaDecay : public TNamed {
 public:
   TBetaDecay();
-  TBetaDecay(TNucleus*);
-  ~TBetaDecay(){};
+  TBetaDecay(TNucleus *parent);
+  TBetaDecay(char* name);
+  TBetaDecay(Int_t Z, Int_t N);
+  ~TBetaDecay();
 
-  TNucleus *GetParent() const {return parent;}
+public:
+  TNucleus *GetParent() const {return fparent;}
 
 private:
 
-  TNucleus *parent; //The parent nucleus beta decaying
+  TNucleus *fparent = NULL; //The parent nucleus beta decaying
  
 
 
--- libraries/TGRSIAnalysis/TBetaDecay/TBetaDecay.cxx
+++ libraries/TGRSIAnalysis/TBetaDecay/TBetaDecay.cxx
@@ -5,8 +5,29 @@ ClassImp(TBetaDecay);
 
 TBetaDecay::TBetaDecay(){}
 
-TBetaDecay::TBetaDecay(TNucleus *par):parent(par){
+TBetaDecay::TBetaDecay(TNucleus *parent):fparent(parent){
+
+//This is where we want to open parameter files,etc.
+
 
 
 }
 
+TBetaDecay::TBetaDecay(char *name){
+   
+   TNucleus *fparent = new TNucleus(name);
+
+}
+
+TBetaDecay::TBetaDecay(Int_t Z, Int_t N){
+
+   TNucleus *fparent = new TNucleus(Z,N);
+
+}
+
+
+TBetaDecay::~TBetaDecay(){
+
+   delete fparent;
+
+}
