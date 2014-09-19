9e9a65fefb582003b0199b115ac1fcb972ba4772
r3dunlop <r3dunlop@gmail.com>
2014-09-19 18:29:31 -0400

    removed html test

--- util/html_generator.C
+++ util/html_generator.C
@@ -112,7 +112,7 @@ void html_generator() {
    gSystem->Load("libTreePlayer");
    gSystem->Load("$GRSISYS/libraries/libMidasFormat.so");
    gSystem->Load("$GRSISYS/libraries/libGRSIFormat.so");
-   gSystem->Load("$GRSISYS/libraries/libAnalysisTreeBuilder.so");
+ //  gSystem->Load("$GRSISYS/libraries/libAnalysisTreeBuilder.so");
  //  gSystem->Load("$GRSISYS/libraries/libGRSIRootIO.so");
    gSystem->Load("$GRSISYS/libraries/libGriffin.so");
    gSystem->Load("$GRSISYS/libraries/libCSM.so");
