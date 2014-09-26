788a935473697af10837aa5d6db4a49c8eb2fe16
r3dunlop <r3dunlop@gmail.com>
2014-09-25 20:33:41 -0400

    Added skeleton TECal - Removed exteranneous TTransition and TEnergyLevel

---
+++ include/TECal.h
@@ -0,0 +1,12 @@
+#include <TObject.h>
+
+class TECal : public TObject {
+ public: 
+   TECal(){};
+   ~TECal(){}; 
+
+
+
+  ClassDef(TECal,1);
+
+};
---
+++ libraries/TGRSIAnalysis/TECal/TECal.cxx
@@ -0,0 +1,3 @@
+#include "TECal.h"
+
+ClassImp(TECal)
