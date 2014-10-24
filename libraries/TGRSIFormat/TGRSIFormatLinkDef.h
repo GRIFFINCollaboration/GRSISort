91dc962222563652632150c5914471c5fa5370b7
P.C. Bender <pcbend@gmail.com>
2014-10-24 13:27:51 -0700

    master dictionary/linkdef for grsiformat

---
+++ libraries/TGRSIFormat/TGRSIFormatLinkDef.h
@@ -0,0 +1,25 @@
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+
+#pragma link C++ class std::vector<Int_t>+;
+#pragma link C++ class std::vector<Short_t>+;
+#pragma link C++ class std::vector<UShort_t>+;
+
+#pragma link C++ class TFragment+;
+#pragma link C++ class TChannel+;
+#pragma link C++ class TGRSIRunInfo+;
+#pragma link C++ class TGRSIStats+;
+#pragma link C++ class TFragmentQueue+;
+#pragma link C++ class TFragmentSelector+;
+
+#endif
+
+
+
+
+
