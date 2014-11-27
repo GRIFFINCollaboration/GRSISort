401711837e4813424b4249782bd5b6626f1d6c38
P.C. Bender <pcbend@gmail.com>
2014-11-26 16:36:10 -0800

    Added LinkDefs for real this time

---
+++ libraries/TGRSIAnalysis/TSceptar/TSceptarLinkDef.h
@@ -0,0 +1,18 @@
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+
+
+#pragma link C++ class TSceptarHit+;
+#pragma link C++ class TSceptar+;
+
+#endif
+
+
+
+
+
