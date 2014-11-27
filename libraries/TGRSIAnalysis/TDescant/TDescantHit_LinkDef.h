a954c738f1ee8175e3269c6f4ca87a5411f50d08
P.C. Bender <pcbend@gmail.com>
2014-11-26 16:40:36 -0800

    Removed unnecessary _LinkDef Files

--- libraries/TGRSIAnalysis/TDescant/TDescantHit_LinkDef.h
+++
@@ -1,9 +0,0 @@
-#ifdef __CINT__
-//#pragma link off all globals;
-//#pragma link off all classes;
-//#pragma link off all functions;
-//#pragma link C++ nestedclasses;
-#pragma link C++ class TDescantHit+;
-//#pragma link C++ class std::vector<Short_t>+;
-//#pragma read sourceClass="TDescantHit" target="waveform" attributes="Owner";
-#endif
--- libraries/TGRSIAnalysis/TPaces/TPacesHit_LinkDef.h
+++
@@ -1,9 +0,0 @@
-#ifdef __CINT__
-//#pragma link off all globals;
-//#pragma link off all classes;
-//#pragma link off all functions;
-//#pragma link C++ nestedclasses;
-#pragma link C++ class TPacesHit+;
-#pragma link C++ class std::vector<TCrystalHit>+;
-//#pragma link C++ class std::vector<Short_t>+;
-#endif
