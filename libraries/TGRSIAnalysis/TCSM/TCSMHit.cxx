44add71e1ee05dbdb8843b3d75782902fbcba8bd
Ryan Braid <rbraid@mines.edu>
2015-04-08 16:25:15 -0600

    Made GetDEnergy and GetEEnergy more intelligent on what strips it trusts

--- libraries/TGRSIAnalysis/TCSM/TCSMHit.cxx
+++ libraries/TGRSIAnalysis/TCSM/TCSMHit.cxx
@@ -44,6 +44,122 @@ void TCSMHit::Clear(Option_t *options)	{
    detectornumber = 0;	//
 }
 
+Double_t TCSMHit::GetDEnergy()
+{
+  bool trustVertical = 1;
+  bool trustHoriztonal = 1;
+  
+  switch(GetDetectorNumber())
+  {
+    case 1:
+    switch(GetDHorizontalStrip())
+    {
+      case 6:
+      trustHoriztonal = 0;
+      break;
+    }
+    break;
+
+    case 2:
+    switch(GetDHorizontalStrip())
+    {
+      //case 8:
+      case 9:
+      //case 10:
+      trustHoriztonal = 0;
+      break;
+    }
+    switch(GetDVerticalStrip())
+    {
+      case 5:
+      trustVertical = 0;
+      break;
+    }
+    break;
+
+    case 3:
+      switch(GetDHorizontalStrip())
+      {
+	case 12:
+	case 15:
+	  trustHoriztonal = 0;
+	  break;
+      }
+    break;
+
+    case 4:
+
+    break;
+
+    default:
+    std::cerr<<"  ERROR, Trying to get energy from a D detector that doesn't exist!"<<std::endl;
+  }
+
+  if(trustVertical && trustHoriztonal)
+    return((GetDVerticalEnergy() + GetDHorizontalEnergy())/2.);
+  else if(trustVertical && !trustHoriztonal)
+    return(GetDVerticalEnergy());
+  else if(!trustVertical && trustHoriztonal)
+    return(GetDHorizontalEnergy());
+  else if(!trustVertical && !trustVertical)
+    return(0.);
+  else
+    std::cerr<<"  ERROR, I don't know who to trust in GetDEnergy()"<<std::endl;
+}
+
+Double_t TCSMHit::GetEEnergy()
+{
+  bool trustVertical = 1;
+  bool trustHoriztonal = 1;
+  
+  switch(GetDetectorNumber())
+  {
+    case 1:
+
+      break;
+
+    case 2:
+      switch(GetEHorizontalStrip())
+      {
+	case 0:
+	case 2:
+	case 3:
+	case 6:
+	//case 8:
+	case 12:
+	//case 15:
+	trustHoriztonal = 0;
+	break;
+      }
+      switch(GetEVerticalStrip())
+      {
+	case 0:
+	trustVertical = 0;
+	break;
+      }
+      break;
+
+    case 3:
+      break;
+
+    case 4:
+      break;
+
+    default:
+      std::cerr<<"  ERROR, Trying to get energy from a E detector that doesn't exist!"<<std::endl;
+  }
+  
+  if(trustVertical && trustHoriztonal)
+    return((GetEVerticalEnergy() + GetEHorizontalEnergy())/2.);
+  else if(trustVertical && !trustHoriztonal)
+    return(GetEVerticalEnergy());
+  else if(!trustVertical && trustHoriztonal)
+    return(GetEHorizontalEnergy());
+  else if(!trustVertical && !trustVertical)
+    return(0.);
+  else
+    std::cerr<<"  ERROR, I don't know who to trust in GetEEnergy()"<<std::endl;}
+
 void TCSMHit::Print(Option_t *options)	{
   std::cout<<"Printing TCSMHit:  Horizontal    Vertical"<<std::endl;
   std::cout<<"Detector number: "<<GetDetectorNumber()<<std::endl;
