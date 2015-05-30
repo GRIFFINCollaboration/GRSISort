baba2856388d3523928235eb6050c2ea81b2f76e
P.C. Bender <pcbend@gmail.com>
2015-05-29 22:20:22 -0700

    Removed copy of parent

--- libraries/TGRSIAnalysis/TGRSIDetector/TGRSIDetectorHit.cxx
+++ libraries/TGRSIAnalysis/TGRSIDetector/TGRSIDetectorHit.cxx
@@ -61,7 +61,7 @@ void TGRSIDetectorHit::Copy(TGRSIDetectorHit &rhs) const {
   ((TGRSIDetectorHit&)rhs).charge   = charge;
   ((TGRSIDetectorHit&)rhs).detector = detector;
   ((TGRSIDetectorHit&)rhs).energy   = energy;
-  ((TGRSIDetectorHit&)rhs).parent  = parent;  
+//  ((TGRSIDetectorHit&)rhs).parent  = parent;  
 }
 
 void TGRSIDetectorHit::Print(Option_t *opt) const {
