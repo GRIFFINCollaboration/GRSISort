2ac57eca9022dfd982cbabb66eef0aa11ebd8ebe
VinzenzBildstein <vbildste@uoguelph.ca>
2015-07-08 18:00:00 -0400

    Added Zc, CcShort, and CcLong to DescantData/DescantHit. Fixed bug with Cfd parameter.

--- include/TDescantData.h
+++ include/TDescantData.h
@@ -19,6 +19,9 @@ class TDescantData : public TGRSIDetectorData {
       std::vector<Double_t> fDet_Eng;		//!
 		std::vector<Int_t>    fDet_Chg;		//!
 		std::vector<Int_t>    fDet_CFD;		//!
+		std::vector<Int_t>    fDet_Zc;		//!
+		std::vector<Int_t>    fDet_CcShort;		//!
+		std::vector<Int_t>    fDet_CcLong;		//!
 		std::vector<Long_t>   fDet_Time;		//!
 		std::vector<std::vector<Short_t> >fDet_Wave;	//!
 
@@ -40,6 +43,9 @@ class TDescantData : public TGRSIDetectorData {
       inline void SetDetEnergy(const Double_t  &DetEnergy)	   {fDet_Eng.push_back(DetEnergy);      }	//!
 		inline void SetDetCharge(const Int_t &DetCharge)	      {fDet_Chg.push_back(DetCharge);      }	//!
 		inline void SetDetCFD(const Int_t &DetCFD)	            {fDet_CFD.push_back(DetCFD); }	//!	
+		inline void SetDetZc(const Int_t &DetZc)	            {fDet_Zc.push_back(DetZc); }	//!	
+		inline void SetDetCcShort(const Int_t &DetCcShort)	            {fDet_CcShort.push_back(DetCcShort); }	//!	
+		inline void SetDetCcLong(const Int_t &DetCcLong)	            {fDet_CcLong.push_back(DetCcLong); }	//!	
 		inline void SetDetTime(const Long_t    &DetTime)	      {fDet_Time.push_back(DetTime);       }	//!
 
 		inline void SetWave(const std::vector<Short_t> &DetWave)	{fDet_Wave.push_back(DetWave);} //!
@@ -55,6 +61,21 @@ class TDescantData : public TGRSIDetectorData {
             SetDetEnergy(frag->GetEnergy(x));
 		      SetDetCharge(frag->GetCharge(x));
 		      SetDetCFD(frag->Cfd.at(x));	 
+				if(x < frag->Zc.size()) {
+					SetDetZc(frag->Zc.at(x));	 
+				} else {
+					SetDetZc(0);
+				}
+				if(x < frag->ccShort.size()) {
+					SetDetCcShort(frag->ccShort.at(x));	 
+				} else {
+					SetDetCcShort(0);
+				}
+				if(x < frag->ccLong.size()) {
+					SetDetCcLong(frag->ccLong.at(x));	 
+				} else {
+					SetDetCcLong(0);
+				}
 		      SetDetTime(frag->GetTimeStamp());	
             if(fSetWave)
    		      SetWave(frag->wavebuffer);
@@ -66,6 +87,9 @@ class TDescantData : public TGRSIDetectorData {
       inline Double_t GetDetEnergy(const unsigned int &i)	   {return fDet_Eng.at(i);}	//!
 		inline Int_t    GetDetCharge(const unsigned int &i)	   {return fDet_Chg.at(i);}	//!
 		inline Int_t    GetDetCFD(const unsigned int &i)	      {return fDet_CFD.at(i);}	//!
+		inline Int_t    GetDetZc(const unsigned int &i)	      {return fDet_Zc.at(i);}	//!
+		inline Int_t    GetDetCcShort(const unsigned int &i)	      {return fDet_CcShort.at(i);}	//!
+		inline Int_t    GetDetCcLong(const unsigned int &i)	      {return fDet_CcLong.at(i);}	//!
 		inline Long_t   GetDetTime(const unsigned int &i)	      {return fDet_Time.at(i);}	//!
 
 		inline std::vector<Short_t> GetDetWave(const unsigned int &i)	{if(fSetWave) { return fDet_Wave.at(i);} else {std::vector<Short_t> w; return w;}}	//!
