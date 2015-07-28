c2f058978d0d25eb64966465fa6a205ee5c037d2
Ryan Dunlop <r3dunlop@gmail.com>
2015-07-28 19:17:42 -0400

    Changed Return statements in TFragment for charge

--- include/TFragment.h
+++ include/TFragment.h
@@ -71,7 +71,7 @@ public:
    double GetTZero() const; //!
    const char *GetName() const; //!
    double GetEnergy(int iter=0)const; //!
-   double GetCharge(int iter=0)const; //!
+   Float_t GetCharge(int iter=0)const; //!
    long GetTimeStamp_ns(); //!
 
    Int_t Get4GCfd(int i=0); //!
