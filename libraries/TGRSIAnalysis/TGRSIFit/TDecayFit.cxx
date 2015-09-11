c5c0bb752a5f5478e2f2b5a90916dd44d460fba2
Ryan Dunlop <r3dunlop@gmail.com>
2015-09-11 12:38:05 -0400

    Can fit nucleus in a decay chain...well

--- libraries/TGRSIAnalysis/TGRSIFit/TDecayFit.cxx
+++ libraries/TGRSIAnalysis/TGRSIFit/TDecayFit.cxx
@@ -4,14 +4,16 @@ ClassImp(TDecay)
 ClassImp(TDecayChain)
 ClassImp(TDecayFit)
 
-TDecay::TDecay() : fParent(0), fDaughter(0), fDecayFunc(0), fGeneration(1) {
+TDecay::TDecay() : fParent(0), fDaughter(0), fDecayFunc(0), fGeneration(1), fDetectionEfficiency(1.0) {
    fFirstParent = this;
    fDecayFunc = new TF1("decayfunc",this,&TDecay::ActivityFunc,0,10,2,"TDecay","ActivityFunc");
    fDecayFunc->SetParameters(0.0,0.0);
    fDecayFunc->SetParNames("Intensity","DecayRate");
+   fTotalDecayFunc = new TF1(*fDecayFunc);
+   SetTotalDecayParameters();
 }
 
-TDecay::TDecay(UInt_t generation, TDecay* parent) : fParent(0), fDaughter(0), fDecayFunc(0){
+TDecay::TDecay(UInt_t generation, TDecay* parent) : fParent(0), fDaughter(0), fDecayFunc(0), fDetectionEfficiency(1.0){
    if(parent){
       fParent = parent;
       fParent->SetDaughterDecay(this);
@@ -39,11 +41,39 @@ TDecay::TDecay(UInt_t generation, TDecay* parent) : fParent(0), fDaughter(0), fD
    fDecayFunc = new TF1("decayfunc",this,&TDecay::ActivityFunc,0,10,2,"TDecay","ActivityFunc");
    fDecayFunc->SetParameters(fFirstParent->GetIntensity(),0.0);
    fDecayFunc->SetParNames("Intensity","DecayRate");
+
+   fTotalDecayFunc = new TF1("totaldecayfunc",this,&TDecay::ActivityFunc,0,10,fGeneration+1,"TDecay","ActivityFunc");
+   SetTotalDecayParameters();
+   if(fFirstParent != this)
+      FixIntensity(0);
 }
 
 TDecay::~TDecay() {
-   if(fDecayFunc)
-      delete fDecayFunc;
+   if(fDecayFunc) delete fDecayFunc;
+   if(fTotalDecayFunc) delete fTotalDecayFunc;
+}
+
+void TDecay::SetTotalDecayParameters() {
+   //Sets the total fit function to know about the other parmaters in the decay chain.
+   Double_t low_limit,high_limit;
+   
+   //We need to include the fact that we have parents and use that TF1 to perform the fit.
+   fTotalDecayFunc->SetParameter(0,fFirstParent->GetIntensity());
+   fTotalDecayFunc->SetParName(0,"Intensity1");
+   fTotalDecayFunc->SetParNames("Intensity","DecayRate1");
+   fFirstParent->GetDecayFunc()->GetParLimits(0,low_limit,high_limit);
+   fTotalDecayFunc->SetParLimits(0,low_limit,high_limit);
+   //Now we need to get the parameters for each of the parents
+   Int_t parCounter = 1;
+   TDecay* curDecay = fFirstParent;
+   while(curDecay){
+      fTotalDecayFunc->SetParameter(parCounter,curDecay->GetDecayRate());
+      fTotalDecayFunc->SetParName(parCounter,Form("DecayRate%d",parCounter));
+      curDecay->GetDecayFunc()->GetParLimits(1,low_limit,high_limit);
+      fTotalDecayFunc->SetParLimits(parCounter,low_limit,high_limit);
+      ++parCounter;
+      curDecay = curDecay->GetDaughterDecay();
+   }
 }
 
 TDecay* const TDecay::GetParentDecay(){
@@ -55,28 +85,26 @@ TDecay* const TDecay::GetDaughterDecay(){
 }
 
 void TDecay::Draw(Option_t* option){
+   SetTotalDecayParameters();
    fDecayFunc->Draw(option);
 }
 
 Double_t TDecay::Eval(Double_t t){
+   SetTotalDecayParameters();
    return fDecayFunc->Eval(t);
 }
 
 Double_t TDecay::ActivityFunc(Double_t *dim, Double_t *par){
    //The general function for a decay chain
    //par[0] is the intensity
-   //par[1] is the activity
+   //par[1*i] is the activity
    Double_t result = 1.0;
    UInt_t gencounter = 0;
    TDecay *curDecay = this;
    //Compute the first multiplication
    while(curDecay){
       ++gencounter;
-
-      if(curDecay == this)
-         result *= par[1];
-      else
-         result *= curDecay->GetDecayRate();
+      result *= par[gencounter];
 
       curDecay = curDecay->GetParentDecay();
    }
@@ -86,11 +114,7 @@ Double_t TDecay::ActivityFunc(Double_t *dim, Double_t *par){
    }
    //Multiply by the initial intensity of the intial parent.
 
-   fFirstParent->SetIntensity(par[0]);
-   if(fFirstParent == this)
-      result*=fFirstParent->GetIntensity()/par[1];
-   else
-      result*=fFirstParent->GetIntensity()/fFirstParent->GetDecayRate();
+   result*=par[0]/par[1];
 
    //Now we need to deal with the second term
    Double_t sum = 0.0;
@@ -100,45 +124,58 @@ Double_t TDecay::ActivityFunc(Double_t *dim, Double_t *par){
       TDecay* denomDecay = this;
       while(denomDecay){
          if(denomDecay != curDecay){ 
-       
-            if(denomDecay == this)
-               denom*=par[1] - curDecay->GetDecayRate(); 
-            else if (curDecay == this)
-               denom*=denomDecay->GetDecayRate() - par[1]; 
-            else
-               denom*=denomDecay->GetDecayRate() - curDecay->GetDecayRate(); 
-
+            denom*=par[denomDecay->GetGeneration()] - par[curDecay->GetGeneration()]; 
          } 
          denomDecay = denomDecay->GetParentDecay();
       }
 
-      if(curDecay == this)
-         sum+=TMath::Exp(-par[1]*dim[0])/denom; 
-      else
-         sum+=TMath::Exp(-curDecay->GetDecayRate()*dim[0])/denom; 
+      sum+=TMath::Exp(-par[curDecay->GetGeneration()]*dim[0])/denom; 
 
       curDecay = curDecay->GetParentDecay();
    }
    result*=sum;
    
-   return result;
+   return result*GetEfficiency();
 
 }
 
 TFitResultPtr TDecay::Fit(TH1* fithist) {
-   TFitResultPtr fitres = fithist->Fit(fDecayFunc,"LRSME");
+   Int_t parCounter = 1;
+   TDecay* curDecay = fFirstParent;
+   SetTotalDecayParameters();
+   TFitResultPtr fitres = fithist->Fit(fTotalDecayFunc,"LRSME");
    Double_t chi2 = fitres->Chi2();
    Double_t ndf = fitres->Ndf();
 
    printf("Chi2/ndf = %lf\n",chi2/ndf);
+
+   //Now copy the fits back to the appropriate nuclei.
+   fFirstParent->SetIntensity(fTotalDecayFunc->GetParameter(0)); fFirstParent->SetIntensityError(fTotalDecayFunc->GetParError(0));
+   //Now we need to set the parameters for each of the parents
+   while(curDecay){
+      curDecay->SetDecayRate(fTotalDecayFunc->GetParameter(parCounter)); curDecay->SetDecayRateError(fTotalDecayFunc->GetParError(parCounter));
+      curDecay = curDecay->GetDaughterDecay();
+      ++parCounter;
+   }
    
    return fitres;
 
 }
 
+void TDecay::Fix(){
+   FixHalfLife();
+   FixIntensity();
+}
+
+void TDecay::Release() {
+   ReleaseHalfLife();
+   ReleaseIntensity();
+}
+
 void TDecay::Print(Option_t *option) const{
-   printf("Intensity: %lf +/- %lf c/s\n", GetIntensity(), GetIntensityError());
-   printf(" HalfLife: %lf +/- %lf s\n", GetHalfLife(), GetHalfLifeError());
+   printf(" Intensity: %lf +/- %lf c/s\n", GetIntensity(), GetIntensityError());
+   printf("  HalfLife: %lf +/- %lf s\n", GetHalfLife(), GetHalfLifeError());
+   printf("Efficiency: %lf\n",GetEfficiency());
    if(fParent)
       printf("Parent Address: %p\n", fParent);
    if(fDaughter)
