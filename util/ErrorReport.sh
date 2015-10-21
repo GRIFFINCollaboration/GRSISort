1e6dd57320e57afad0d8bc3bab94571a762f5c2b
Ryan Dunlop <rdunlop@uoguelph.ca>
2015-10-21 11:45:31 -0400

    Fixes for getting into GRSISYS in error report

--- util/ErrorReport.sh
+++ util/ErrorReport.sh
@@ -1,4 +1,4 @@
-# Source this script to help report Errors in GRSISort
+# Run this script to help report Errors in GRSISort
 # Author by Ryan Dunlop, 22/10/2015
 echo SYSTEM       = `uname`
 echo GRSISYS      = $GRSISYS
@@ -7,7 +7,8 @@ echo ROOTSYS      = $ROOTSYS
 echo ROOT Version = `root-config --version`
 echo '\nComputer and Path to File that failed: \n'
 
-cd ${GRSISYS}
+LASTDIR=$PWD
+cd $GRSISYS
 echo GRSISort Branch = `git rev-parse --abbrev-ref HEAD`
 echo '\nLast Commit: ' 
 
@@ -18,3 +19,4 @@ echo '\nPut Error Here:\n\n\n\n'
 echo 'Last Working Commit: Put commit here\n\n'
 
 echo 'What I have tried so far\n\n\n'
+cd $LASTDIR
