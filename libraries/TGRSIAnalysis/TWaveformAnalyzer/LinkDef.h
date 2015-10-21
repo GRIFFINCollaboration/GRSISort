3e8b2b40d5370faa20ee3b8f34c6d721628300f1
jsmallcombe <jsmallcombe@triumf.ca>
2015-10-20 23:29:38 -0700

    ACTUALLY Added TWaveformAnalyser class for the use of TSiLiHit

---
+++ libraries/TGRSIAnalysis/TWaveformAnalyzer/LinkDef.h
@@ -0,0 +1,18 @@
+// TWaveformAnalyzer.h
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+
+//#pragma link C++ class std::vector<Short_t>+;
+
+#pragma link C++ class TWaveformAnalyzer+;
+
+#endif
+
+
+
+
+
