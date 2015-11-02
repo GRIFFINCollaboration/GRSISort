a6f63c2d1b7b65764c8e04c4b59e77eb0d89175d
jsmallcombe <jsmallcombe@triumf.ca>
2015-11-02 08:52:05 -0800

    Update TRF.cxx

--- libraries/TGRSIAnalysis/TRF/TRF.cxx
+++ libraries/TGRSIAnalysis/TRF/TRF.cxx
@@ -19,10 +19,10 @@ void TRF::FillData(TFragment *frag,TChannel *channel,MNEMONIC *mnemonic) {
 	data->FindPhase((TFragment&)(*frag));
 	//TRF::Set();
 	
-	TPulseAnalyzer pulse((TFragment&)(*frag));	    
-	if(pulse.IsSet()){
-		phasesfu = pulse.fit_newT0();
-	}
+//	TPulseAnalyzer pulse((TFragment&)(*frag));	    
+//	if(pulse.IsSet()){
+//		phasesfu = pulse.fit_newT0();
+//	}
 }
 
 
@@ -44,7 +44,7 @@ void TRF::Clear(Option_t *opt)	{
   if(data) data->Clear(); //!
 		
    phase     = -1.0; 
-   phasesfu  = -1.0; 
+   //phasesfu  = -1.0; 
    midastime =  0.0;
    timestamp =  0.0;
    time      =  0.0;
