8232b0c638148a37dab674487448efedcb6ea8a1
jsmallcombe <jsmallcombe@triumf.ca>
2015-11-03 18:39:28 -0800

    Update TRF.cxx

--- libraries/TGRSIAnalysis/TRF/TRF.cxx
+++ libraries/TGRSIAnalysis/TRF/TRF.cxx
@@ -16,7 +16,7 @@ void TRF::Copy(TObject &rhs) const {
   TDetector::Copy(rhs);
   static_cast<TRF&>(rhs).midastime     = midastime;
   static_cast<TRF&>(rhs).timestamp     = timestamp;
-  static_cast<TRF&>(rhs).timesfu    = timesfu;
+  static_cast<TRF&>(rhs).time    = time;
   return;                                      
 }  
 
@@ -30,7 +30,7 @@ void TRF::FillData(TFragment *frag,TChannel *channel,MNEMONIC *mnemonic) {
 	
 	TPulseAnalyzer pulse((TFragment&)(*frag));	    
 	if(pulse.IsSet()){
-		timesfu = pulse.fit_rf(period_ns*0.2);//period givin in half ticks... for reasons
+		time = pulse.fit_rf(period_ns*0.2);//period givin in half ticks... for reasons
 		midastime = frag->MidasTimeStamp;
 		timestamp = frag->GetTimeStamp();		
 	}
@@ -44,7 +44,7 @@ void TRF::BuildHits(TDetectorData *ddata,Option_t *opt)	{
 void TRF::Clear(Option_t *opt)	{
    midastime =  0.0;
    timestamp =  0.0;
-   timesfu   =  0.0;
+   time   =  0.0;
 }
 
 void TRF::Print(Option_t *opt) const { } 
