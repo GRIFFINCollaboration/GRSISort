750824fd4a69a54b324cd2bf6577f49b40a85f17
jsmallcombe <jsmallcombe@triumf.ca>
2015-11-03 18:17:24 -0800

    Update TRF.h

--- include/TRF.h
+++ include/TRF.h
@@ -22,19 +22,10 @@ class TRF :  public TDetector {
 	TRF(const TRF&);
 	virtual ~TRF();
 		
-	Double_t TimeSFU()const     { return ((timesfu/period_ns)-(int)((timesfu/period_ns)*2.0))*-2*period_ns; }
+	Double_t Time() const     { return timesfu;}
 	Long_t   TimeStamp() const  { return timestamp; }	
 	time_t   MidasTime() const  { return midastime; }
-	Double_t Time() const       { 
-					if(timesfu>period_ns*0.5)	
-					return (1-(timesfu/period_ns))*2*period_ns;
-					else return timesfu*-2;
-				    }
-	Double_t Phase() const      { 	
-					if(timesfu>period_ns*0.5)
-					return (1-(timesfu/period_ns))*TMath::TwoPi();
-					else return(timesfu/period_ns)*-TMath::TwoPi();
-				    }
+	Double_t Phase() const      { return (timesfu/period_ns))*TMath::TwoPi(); }
 	
 	
 	void BuildHits(TDetectorData *data=0, Option_t * = "");
