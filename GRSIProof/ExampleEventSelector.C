6befe89cf19823b0c33b86939acd9aad3e014524
VinzenzBildstein <vbildste@uoguelph.ca>
2016-11-08 17:20:03 -0500

    Moved histograms into map within class
    The histograms are not declared globally, instead they are now stored
    in maps (one for TH1, one for TH2, and one for THnSparseF). This means
    they only have to be initialized and then filled within the .C-file.
    
    Tested this on some data and run times before the change were 38.0,
    34.6, and 37.6 seconds, after the change they were 35.2, 36.2, and
    35.2 s. Seems like the comparison on the const char* is not an issue.

--- GRSIProof/ExampleEventSelector.C
+++ GRSIProof/ExampleEventSelector.C
@@ -4,24 +4,25 @@
 
 #include "TH1.h"
 
-//need to define these histograms either globally or as members of the class
-TH1D* gE;
-TH1D* gE_b;
+void ExampleEventSelector::CreateHistograms() {
+   fH1["gE"] = new TH1D("gE","#gamma Singles",12000,0,3000);
+   fH1["gE_b"] = new TH1D("gE_b","#gamma Singles in rough #beta coincidence",12000,0,3000);
 
-void ExampleEventSelector::CreateHistograms()
-{
-   gE = new TH1D("gE","#gamma Singles",12000,0,3000);
-   gE_b = new TH1D("gE_b","#gamma Singles in rough #beta coincidence",12000,0,3000);
-   GetOutputList()->AddAll(gDirectory->GetList());
+	for(auto it : fH1) {
+		GetOutputList()->Add(it.second);
+	}
+	for(auto it : fH2) {
+		GetOutputList()->Add(it.second);
+	}
+	for(auto it : fHSparse) {
+		GetOutputList()->Add(it.second);
+	}
 }
 
-void ExampleEventSelector::FillHistograms()
-{
+void ExampleEventSelector::FillHistograms() {
    for(auto i = 0; i < fGrif->GetMultiplicity(); ++i){
-      gE->Fill(fGrif->GetGriffinHit(i)->GetEnergy());
+      fH1["gE"]->Fill(fGrif->GetGriffinHit(i)->GetEnergy());
       if(fScep->GetMultiplicity())
-         gE_b->Fill(fGrif->GetGriffinHit(i)->GetEnergy());
+         fH1["gE_b"]->Fill(fGrif->GetGriffinHit(i)->GetEnergy());
    }
-
-
 }
