e6a31b0fa73d432fa43d509b1c00814cefa75999
VinzenzBildstein <vbildste@uoguelph.ca>
2016-11-17 10:24:37 -0500

    Fixed GValue to be able to read single line entries

--- libraries/GROOT/GValue.cxx
+++ libraries/GROOT/GValue.cxx
@@ -201,121 +201,127 @@ int GValue::ReadValFile(const char* filename,Option_t *opt) {
 //  Info  :
 //}
 int GValue::ParseInputData(std::string input, EPriority priority, Option_t *opt) {
-  std::istringstream infile(input);
-  GValue *value = 0;
-  std::string line;
-  int linenumber = 0;
-  int newvalues = 0;
+	std::istringstream infile(input);
+	GValue *value = 0;
+	std::string line;
+	int linenumber = 0;
+	int newvalues = 0;
 
-  bool brace_open = false;
-  std::string name;
+	bool brace_open = false;
+	std::string name;
 
-  while(std::getline(infile,line)) {
-    linenumber++;
-    trim(&line);
-    size_t comment = line.find("//");
-    if(comment != std::string::npos) {
-       line = line.substr(0,comment);
-    }
-    if(line.length()==0)
-      continue;
-    size_t openbrace  = line.find("{");
-    size_t closebrace = line.find("}");
-    size_t colon      = line.find(":");
+	while(std::getline(infile,line)) {
+		linenumber++;
+		trim(&line);
+		size_t comment = line.find("//");
+		if(comment != std::string::npos) {
+			line = line.substr(0,comment);
+		}
+		if(line.length()==0)
+			continue;
+		size_t openbrace  = line.find("{");
+		size_t closebrace = line.find("}");
+		size_t colon      = line.find(":");
 
-    //=============================================//
-    if(openbrace  == std::string::npos &&
-      closebrace == std::string::npos &&
-      colon      == std::string::npos)
-      continue;
-    //=============================================//
-    if(closebrace != std::string::npos) {
-      brace_open = false;
-      if(value) {
-        //Check whether value is in vector. If it isn't add it.
-        GValue *cur_value = FindValue(value->GetName());
-        if(!cur_value) {
-          AddValue(value);
-          newvalues++;
-        } else {
-          value->AppendValue(cur_value);
-          delete value;
-          newvalues++;
-        }
-      }
-      value =0;
-      name.clear();
-    }
-    //=============================================//
-    if(openbrace != std::string::npos) {
-       brace_open = true;
-       name = line.substr(0,openbrace).c_str();
-       trim(&name);
-       value = new GValue(name.c_str());
-    }
-    //=============================================//
-    if(brace_open) {
-      if(colon != std::string::npos) {
-        std::string type = line.substr(0,colon);
-        line = line.substr(colon+1,line.length());
-        trim(&line);
-        std::istringstream ss(line);
-        int j=0;
-        while(type[j]) {
-           char  c = *(type.c_str()+j);
-           c = toupper(c);
-           type[j++] = c;
-        }
-        if(type.compare("NAME")==0) {
-          value->SetName(line.c_str());
-        } else if(type.compare("VALUE")==0) {
-          value->SetValue(std::atof(line.c_str()));
-	  value->fPriority = priority;
-        } else if(type.compare("INFO")==0) {
-          value->SetInfo(line.c_str());
-        }
-      }
-    }
-  }
-  if(!strcmp(opt,"debug"))
-     printf("parsed %i lines,\n",linenumber);
-  return newvalues;
+		//=============================================//
+		if(openbrace  == std::string::npos &&
+				closebrace == std::string::npos &&
+				colon      == std::string::npos)
+			continue;
+		//=============================================//
+		if(openbrace != std::string::npos) {
+			brace_open = true;
+			name = line.substr(0,openbrace).c_str();
+			trim(&name);
+			value = new GValue(name.c_str());
+		}
+		//=============================================//
+		if(brace_open) {
+			if(colon != std::string::npos) {
+				std::string type;
+				if(openbrace == std::string::npos || openbrace > colon) {
+					type = line.substr(0,colon);
+				} else {
+					type = line.substr(openbrace+1,colon - (openbrace+1));
+				}
+				line = line.substr(colon+1,line.length());
+				//trim(&line); //this is not needed? VB
+				trim(&type);
+				//std::istringstream ss(line); //this is not used anywhere? VB
+				int j=0;
+				while(type[j]) {
+					char  c = *(type.c_str()+j);
+					c = toupper(c);
+					type[j++] = c;
+				}
+				if(type.compare("NAME")==0) {
+					value->SetName(line.c_str());
+				} else if(type.compare("VALUE")==0) {
+					value->SetValue(std::atof(line.c_str()));
+					value->fPriority = priority;
+				} else if(type.compare("INFO")==0) {
+					value->SetInfo(line.c_str());
+				}
+			}
+		}
+		//=============================================//
+		if(closebrace != std::string::npos) {
+			brace_open = false;
+			if(value) {
+				//Check whether value is in vector. If it isn't add it.
+				GValue *cur_value = FindValue(value->GetName());
+				if(!cur_value) {
+					AddValue(value);
+					newvalues++;
+				} else {
+					value->AppendValue(cur_value);
+					delete value;
+					newvalues++;
+				}
+			}
+			value =0;
+			name.clear();
+		}
+	}
+	if(!strcmp(opt,"debug"))
+		printf("parsed %i lines,\n",linenumber);
+	return newvalues;
 }
 
 void GValue::trim(std::string * line, const std::string & trimChars) {
-   //Removes the the string "trimCars" from  the string 'line'
-  if(line->length() == 0)
-    return;
-  std::size_t found = line->find_first_not_of(trimChars);
-  if(found != std::string::npos)
-    *line = line->substr(found, line->length());
-  found = line->find_last_not_of(trimChars);
-  if(found != std::string::npos)
-    *line = line->substr(0, found + 1);
-  return;
+	//Removes the the string "trimCars" from  the string 'line'
+	if(line->length() == 0)
+		return;
+	std::size_t found = line->find_first_not_of(trimChars);
+	if(found != std::string::npos)
+		*line = line->substr(found, line->length());
+	found = line->find_last_not_of(trimChars);
+	if(found != std::string::npos)
+		*line = line->substr(0, found + 1);
+	return;
 }
 
 
 void GValue::Streamer(TBuffer &R__b) {
-  this->SetBit(kCanDelete);
-  UInt_t R__s, R__c;
-  if(R__b.IsReading()) {
-     Version_t R__v = R__b.ReadVersion(&R__s,&R__c);
-     TNamed::Streamer(R__b);
-     if(R__v>1) { }
-     {
-       TString R__str;
-       R__str.Streamer(R__b);
-       ParseInputData(R__str.Data(),kRootFile);
-     }
-     R__b.CheckByteCount(R__s,R__c,GValue::IsA());
-  } else {
-     R__c = R__b.WriteVersion(GValue::IsA(),true);
-     TNamed::Streamer(R__b);
-     {
-       TString R__str = GValue::WriteToBuffer();
-       R__str.Streamer(R__b);
-     }
-     R__b.SetByteCount(R__c,true);
-  }
+	this->SetBit(kCanDelete);
+	UInt_t R__s, R__c;
+	if(R__b.IsReading()) {
+		Version_t R__v = R__b.ReadVersion(&R__s,&R__c);
+		TNamed::Streamer(R__b);
+		if(R__v>1) { }
+		{
+			TString R__str;
+			R__str.Streamer(R__b);
+			ParseInputData(R__str.Data(),kRootFile);
+		}
+		R__b.CheckByteCount(R__s,R__c,GValue::IsA());
+	} else {
+		R__c = R__b.WriteVersion(GValue::IsA(),true);
+		TNamed::Streamer(R__b);
+		{
+			TString R__str = GValue::WriteToBuffer();
+			R__str.Streamer(R__b);
+		}
+		R__b.SetByteCount(R__c,true);
+	}
 }
