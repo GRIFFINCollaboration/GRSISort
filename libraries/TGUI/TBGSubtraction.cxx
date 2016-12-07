2414b36091949041e5101dcb223e086f2c3a6083
Ryan Dunlop <rdunlop@uoguelph.ca>
2016-12-07 15:12:28 -0500

    Added ability to change histo and file names in gui subtractor

--- libraries/TGUI/TBGSubtraction.cxx
+++ libraries/TGUI/TBGSubtraction.cxx
@@ -96,10 +96,13 @@ void TBGSubtraction::BuildInterface() {
    fBGParamEntry->Connect("ValueSet(Long_t)", "TBGSubtraction", this, "DoDraw()");
    fBGParamEntry->Connect("ValueSet(Long_t)", "TBGSubtraction", this, "DoProjection()");
 
+   fDescriptionFrame       = new TGHorizontalFrame(fGateFrame,200,200);
+   fHistogramDescription   = new TGTextEntry(fDescriptionFrame,"gated #gamma-#gamma",kHistogramDescriptionEntry);
 
    fButtonFrame = new TGHorizontalFrame(fGateFrame,200,200);
-   fDrawCanvasButton = new TGTextButton(fButtonFrame,"&Draw Canvas");
-   fDrawCanvasButton->Connect("Clicked()", "TBGSubtraction", this, "DrawOnNewCanvas()");
+   fWrite2FileName   = new TGTextEntry(fButtonFrame,"default.root",kWrite2FileNameEntry);
+   //fDrawCanvasButton = new TGTextButton(fButtonFrame,"&Draw Canvas");
+   //fDrawCanvasButton->Connect("Clicked()", "TBGSubtraction", this, "DrawOnNewCanvas()");
    fWrite2FileButton = new TGTextButton(fButtonFrame,"&Write Histograms");
    fWrite2FileButton->Connect("Clicked()", "TBGSubtraction", this, "WriteHistograms()");
 
@@ -122,7 +125,10 @@ void TBGSubtraction::BuildInterface() {
    fBGEntryFrame->AddFrame(fBGEntryLow,fBly);
    fBGEntryFrame->AddFrame(fBGEntryHigh,fBly);
 
-   fButtonFrame->AddFrame(fDrawCanvasButton,fBly);
+   //fButtonFrame->AddFrame(fDrawCanvasButton,fBly);
+   fDescriptionFrame->AddFrame(fHistogramDescription,fBly);
+
+   fButtonFrame->AddFrame(fWrite2FileName,fBly);
    fButtonFrame->AddFrame(fWrite2FileButton,fBly);
 
    fProjectionFrame->AddFrame(fProjectionCanvas, fLayoutCanvases);
@@ -133,6 +139,7 @@ void TBGSubtraction::BuildInterface() {
    
    fGateFrame->AddFrame(fGateCanvas, fLayoutCanvases);
    fGateFrame->AddFrame(fBGParamFrame, fLayoutParam);
+   fGateFrame->AddFrame(fDescriptionFrame,fLayoutParam);
    fGateFrame->AddFrame(fButtonFrame,fLayoutParam);
    
    AddFrame(fProjectionFrame, fBly1);
@@ -419,10 +426,32 @@ void TBGSubtraction::DrawOnNewCanvas(){
 }
 
 void TBGSubtraction::WriteHistograms() {
-   TFile f("test.root","Update");
-   std::cout << "Writing histograms to " << f.GetName() << std::endl;
-   if(fSubtractedHist) fSubtractedHist->Write();
-   if(fBGHist) fBGHist->Write();
-   if(fGateHist) fGateHist->Write();
+   //Find if there is a file name
+   const char *file_name = fWrite2FileName->GetText();
+   if(file_name == NULL){
+      std::cout << "Please enter a file name" << std::endl;
+      return;
+   }
+
+   TFile f(file_name,"Update");
+   std::cout << "Writing " << fHistogramDescription->GetText() << " histograms to " << f.GetName() << std::endl;
+   if(fSubtractedHist){
+      if(fHistogramDescription->GetText() != NULL)
+         fSubtractedHist->SetTitle(fHistogramDescription->GetText());
+      fSubtractedHist->Write();
+   }
+
+   if(fBGHist){
+      if(fHistogramDescription->GetText() != NULL)
+         fBGHist->SetTitle(Form("%s Background",fHistogramDescription->GetText()));
+
+      fBGHist->Write();
+   }
+   if(fGateHist){
+      if(fHistogramDescription->GetText() != NULL)
+         fGateHist->SetTitle(Form("%s Gate only",fHistogramDescription->GetText()));
+   
+      fGateHist->Write();
+   }
 }
 
