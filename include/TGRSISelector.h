63ea13ae26154d765bd97667022bb19c532752ae
Ryan Dunlop <r3dunlop@gmail.com>
2017-04-21 11:51:39 -0400

    Moved histogram maps into protected

--- include/TGRSISelector.h
+++ include/TGRSISelector.h
@@ -38,7 +38,7 @@ public :
    Int_t   GetEntry(Long64_t entry, Int_t getall = 0) { return fChain ? fChain->GetTree()->GetEntry(entry, getall) : 0; }
    void    SetOption(const char *option) { fOption = option; }
    void    SetObject(TObject *obj) { fObject = obj; }
-   void    SetInputList(TList *input) { fInput = input; }
+   //void    SetInputList(TList *input) { fInput = input; }
    TList  *GetOutputList() const { return fOutput; }
    void    SlaveTerminate();
    void    Terminate();
@@ -49,11 +49,12 @@ public :
 	virtual void EndOfSort() { }; 
    void SetOutputPrefix(const char* prefix) { fOutputPrefix = prefix; }
 
- private:
+ protected:
 	std::map<std::string, TH1*> fH1;
 	std::map<std::string, TH2*> fH2;
 	std::map<std::string, THnSparseF*> fHSparse;
 
+ private:
    std::string fOutputPrefix;
 
    ClassDef(TGRSISelector,2);
