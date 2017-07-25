2b929a4d7129b074c00ddb8cea7004781e963aeb
Ryan Dunlop <rdunlop@uoguelph.ca>
2017-07-25 11:01:24 -0400

    Refactored TBGSubtraction, Added peak fitting. - Refactored TBGSubtraction to be easier to follow, and easier to update. This could use some more work, but I don't think it's worth the time right now. - Added a peak fitting ability. Should be relatively intuitive.

--- include/TBGSubtraction.h
+++ include/TBGSubtraction.h
@@ -6,6 +6,7 @@
 #include <TRandom.h>
 #include <TGButton.h>
 #include "TGDoubleSlider.h"
+#include "TGTripleSlider.h"
 #include "TGSlider.h"
 #include "TGToolTip.h"
 #include "TClass.h"
@@ -17,6 +18,8 @@
 #include <TRootEmbeddedCanvas.h>
 #include <RQ_OBJECT.h>
 
+#include "TPeak.h"
+
 #include "TH1.h"
 #include "TH2.h"
 #include "GCanvas.h"
@@ -26,12 +29,15 @@ class TBGSubtraction : public TGMainFrame {
    enum ESliders {
       kGateSlider,
       kBGSlider,
-
+      kPeakSlider
    };
    enum EEntries {
       kBGParamEntry,
       kGateLowEntry,
       kGateHighEntry,
+      kPeakLowEntry,
+      kPeakHighEntry,
+      kPeakEntry,
       kBGLowEntry,
       kBGHighEntry,
       kWrite2FileNameEntry,
@@ -52,6 +58,7 @@ private:
    TH1*                 fSubtractedHist;
    TGDoubleHSlider*     fGateSlider;
    TGDoubleHSlider*     fBGSlider;
+   TGTripleHSlider*     fPeakSlider;
    TGNumberEntry*       fBGParamEntry;
    TGNumberEntry*       fBGEntryLow{nullptr};
    TGNumberEntry*       fBGEntryHigh{nullptr};
@@ -69,6 +76,7 @@ private:
    TGTextEntry* fHistogramDescription{nullptr};
    //      TGTextButton         *fDrawCanvasButton;
    TGTextButton* fWrite2FileButton{nullptr};
+   TGTextButton* fPeakFitButton{nullptr};
 
    // Status Bars
    TGStatusBar* fProjectionStatus{nullptr};
@@ -76,6 +84,7 @@ private:
    // Frames
    TGVerticalFrame*   fGateFrame;
    TGVerticalFrame*   fProjectionFrame;
+   TGHorizontalFrame* fPeakFitFrame{nullptr};
    TGHorizontalFrame* fBGParamFrame{nullptr};
    TGHorizontalFrame* fGateEntryFrame{nullptr};
    TGHorizontalFrame* fBGEntryFrame{nullptr};
@@ -90,32 +99,52 @@ private:
    GMarker* fHighGateMarker;
    GMarker* fLowBGMarker;
    GMarker* fHighBGMarker;
+   GMarker* fLowPeakMarker;
+   GMarker* fHighPeakMarker;
+   GMarker* fPeakMarker;
 
    TFile* fCurrentFile;
 
    Int_t fGateAxis;
 
    Bool_t fForceUpdate;
+   Double_t fPeakLowLimit;
+   Double_t fPeakHighLimit;
+   Double_t fPeakLowValue;
+   Double_t fPeakHighValue;
+   Double_t fPeakValue;
+
+   TPeak* fPeakFit;
 
 public:
    TBGSubtraction(TH2* mat, const char* gate_axis = "x");
    ~TBGSubtraction() override;
    void AxisComboSelected();
    void ClickedBGButton();
-   void DoDraw();
-   void DoFit();
+   void DoPeakFit();
    void DoSlider(Int_t pos = 0);
    void DoEntry(Long_t);
-   void DoGateCanvasModified();
+   void DoProjectionCanvasZoomed();
+   void DoGateCanvasZoomed();
+   void UpdateProjectionSliders();
+   void UpdatePeakSliders();
+   void UpdateBackground();
    void DoProjection();
+   void DoGating();
    void DrawOnNewCanvas();
-   void DrawMarkers();
+   void DrawGateMarkers();
+   void DrawBGMarkers();
+   void DrawPeakMarkers();
+   void DrawPeak();
    void WriteHistograms();
    void GateStatusInfo(Int_t event, Int_t px, Int_t py, TObject* selected);
    void ProjectionStatusInfo(Int_t event, Int_t px, Int_t py, TObject* selected);
 
 private:
    void BuildInterface();
+   void ResetInterface();
+   void MakeConnections();
+   void InitializeInterface();
    void StatusInfo(Int_t event, Int_t px, Int_t py, TObject* selected);
 
    /// \cond CLASSIMP
