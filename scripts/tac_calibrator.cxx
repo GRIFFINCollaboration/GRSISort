dbe415cd294e49b7ab59ca0f81ff3aeb70d36bb9
bolaizol <bruno.olaizola@ucm.es>
2017-08-11 13:48:39 -0700

    Updated scripts

--- scripts/tac_calibrator.cxx
+++ scripts/tac_calibrator.cxx
@@ -1,19 +1,20 @@
 //Written by B. Olaizola
-//Date: 2017-07-27
+//Date: 2017-08-10
 
 //ROOT:6.08/06
 //GRSISort:3.1.3.2
 
-//Usage is: tac_calibration /path/to/filenumber.root
+//Usage is: tac_calibration /path/to/datafile.root
 
-//Takes an AnalysisTree, searches for anti-coincidences TAC-LaBr (so we are sure we only have the TAC-calibrator and not LaBr-LaBr timing) and performs a linear fit to the peaks it finds
-//NOTE: The TAC and TAC-calibrator parameters must be edited
-//NOTE2: There are several sections of the code commented. They produce different quality-control plots and files. Consider uncommenting them to double check your results.
+//Takes an AnalysisTree, searches for anti-coincidences TAC-LaBr (so we are sure we only have the TAC-calibrator and not LaBr-LaBr timing) and performs a linear fit to the peaks it finds. The output is a *.cal file with the TAC calibrated.
+
+//NOTE1: The need for LaBr anticoincidences is the reason it has to work at the AnalysisTree level.
+//NOTE2: The TAC and TAC-calibrator parameters must be edited for every experiment (but the default would be 50 ns TAC range and 10 ns calibrator period).
+//NOTE3: There are several sections of the code commented. They produce different quality-control plots and files. Consider uncommenting them to double check your results.
 
 
 #include "TFile.h"
 #include "TTree.h"
-#include "TFragment.h"
 #include "TH1F.h"
 #include "TF1.h"
 #include "TMath.h"
@@ -52,10 +53,11 @@ int main(int argc, char** argv) {
   
 
 ////////////////////////////EDIT THIS PART//////////////////////////////////////////////////////////EDIT THIS PART///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	double calibrator_period=160000.;//The period of the calibrator (manually set in the module). It is the time difference between the peaks, 80 ns in this case
-	double tac_range = 2000000.; //TAC range, 1 microsec in this case
-	double ps_per_chan = 200.; //binning of the TACs, in ps per channel 
-	int first_TAC_channel = 84; //This is the channel number of the first TAC, it is needed to write the calibration file
+	double calibrator_period=10000.;//The period of the calibrator (manually set in the module). It is the time difference between the peaks, which will be 10 ns in most cases
+	double tac_range = 50000.; //TAC range, 50 ns for most experiments
+	double ps_per_chan = 10.; //binning of the TACs, in ps per channel, 10 is a reasonable number 
+	int first_TAC_channel = 84; //This is the channel number of the first TAC, it is needed to write the calibration file. In general it will be 84
+        int number_of_peaks=(int) (tac_range/calibrator_period); //maximum number of peaks that we expect to find in the TAC-calibrator spectra
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -77,9 +79,6 @@ int main(int argc, char** argv) {
    TLaBr* labr = NULL;
    TZeroDegree* zds = NULL;
    TTAC* tac = NULL;
-   //TLaBrHit* labrhit = NULL;
-   //TZeroDegreeHit* zdshit = NULL;
-   //TTACHit* tachit = NULL;
 
 
    if(tree->FindBranch("TLaBr") != 0) {    //We check to see if we have a LaBr branch in the analysis tree
@@ -96,91 +95,80 @@ int main(int argc, char** argv) {
    }
 
 
-   TH1F * test[8]; for(int i=0; i<8; i++){
-	 test[i] = new TH1F(Form("raw_TAC_calibrator%d",i),Form("raw_TAC_calibrator%d",i),(int) (tac_range/ps_per_chan),0,tac_range); list.Add(test[i]);
-	}
-   TH1F * raw_tac[8]; for(int i=0; i<8; i++){
-	 raw_tac[i] = new TH1F(Form("raw_TAC%d",i),Form("raw_TAC%d",i),(int) (tac_range/ps_per_chan),0,tac_range); list.Add(raw_tac[i]);
+   TH1F * raw_tac_calibrator[8]; for(int i=0; i<8; i++){
+	 raw_tac_calibrator[i] = new TH1F(Form("raw_TAC_calibrator%d",i),Form("raw_TAC_calibrator%d",i),(int) (tac_range/ps_per_chan),0,tac_range); list.Add(raw_tac_calibrator[i]);
 	}
 
-//   for(int i=0; i<tree->GetEntries(); i++){ //Loops through the whole AnalysisTree
-   for(int i=0; i<1e6; i++){ //Loops through the whole AnalysisTree
+   //for(int i=0; i<tree->GetEntries(); i++){ //Loops through the whole AnalysisTree
+   for(int i=0; i<1e6; i++){ //Loops through limited number of entries. A million usually is enough
 
 			
-      tree->GetEntry(i);
-/*
-		if(tac->GetMultiplicity()>0){
-			for(int j=0; j< tac->GetMultiplicity(); j++){
-				raw_tac[tac->GetTACHit(j)->GetDetector()-1]->Fill(tac->GetTACHit(j)->GetCharge());
-			}
-			if(labr->GetMultiplicity()==0 && zds->GetMultiplicity()==0){
-				for(int j=0; j< tac->GetMultiplicity(); j++){
-					test[tac->GetTACHit(j)->GetDetector()-1]->Fill(tac->GetTACHit(j)->GetCharge());
-				}
-			}
-		}
-}*/
+     tree->GetEntry(i);
 
-			for(int j=0; j< tac->GetMultiplicity(); j++){
-		      	int histnum = tac->GetTACHit(j)->GetDetector()-1;
- 			     if(histnum >= 0 && histnum <= 7){   
-    			     double charge = tac->GetTACHit(j)->GetCharge();
-    			     raw_tac[histnum]->Fill(charge);
-    			  }
-			}
-		if(tac->GetMultiplicity()>0 && labr->GetMultiplicity()==0 && zds->GetMultiplicity()==0){
-		  //if(tac->GetMultiplicity()>0 && labr->GetMultiplicity()==0){
-			for(int j=0; j< tac->GetMultiplicity(); j++){
-		      	int histnum = tac->GetTACHit(j)->GetDetector()-1;
- 			     if(histnum >= 0 && histnum <= 7){   
-    			     double charge = tac->GetTACHit(j)->GetCharge();
-    			     test[histnum]->Fill(charge);
-    			  }
-			}
-		}
-	}
+     if(tac->GetMultiplicity()>0 && labr->GetMultiplicity()==0){
+       for(int j=0; j< tac->GetMultiplicity(); j++){
+	 int histnum = tac->GetTACHit(j)->GetDetector()-1;
+	 if(histnum >= 0 && histnum <= 7){   
+	   double charge = tac->GetTACHit(j)->GetCharge();
+	   raw_tac_calibrator[histnum]->Fill(charge);
+	 }
+       }
+     }
+   }
 
   
-   int number_of_peaks=(int) (tac_range/calibrator_period);
-	//double time[number_of_peaks], centroid[number_of_peaks];
    TH1F * calibration_hist[8]; for(int i=0; i<8; i++){
-	 calibration_hist[i] = new TH1F(Form("calibration_hist%d",i),Form("calibration_hist%d",i),(tac_range/ps_per_chan),0,tac_range); list.Add(calibration_hist[i]);
-	}
+     calibration_hist[i] = new TH1F(Form("calibration_hist%d",i),Form("calibration_hist%d",i),(tac_range/ps_per_chan),0,tac_range); list.Add(calibration_hist[i]);
+   }
 
    for(int j=0; j<=7; j++){
-      TSpectrum spec(number_of_peaks); //The argument is the number of peaks to be found 
-      //spec.Search(test[j]);
-	  spec.Search(test[j],1,"",0.25);//The 4th argument will discard peaks below 25% of the highest peak. This is important to tune.
+     TSpectrum spec; //It can take an argument as the max number of peaks to be found, but I recomend leaving it blank
+     spec.Search(raw_tac_calibrator[j],1,"",0.10); //The 4th argument will discard peaks below 10% of the highest peak. This is important to tune, in a very high rate the number of LaBr-LaBr coincidences that sneak in could be considerable.
+     Int_t nfound = spec.Search(raw_tac_calibrator[j],1,"",0.10);
+     if(nfound>number_of_peaks){
+       std::cout<<"WARNING!!! Tspectrum found "<<(nfound-number_of_peaks)<<" too many peaks " <<std::endl;//Warning for bad peak found, results should be inspected
+       nfound=number_of_peaks; 
+     }
 
-      std::vector<double> vec;
-      for(int i=0; i<number_of_peaks; i++) vec.push_back(spec.GetPositionX()[i]);
-      std::sort(vec.begin(),vec.end());
+     std::vector<double> vec;
+     vec.resize(nfound);//we make sure we have a vector with the right number of peaks
+     for(int i=0; i<nfound; i++) vec.at(i)=spec.GetPositionX()[i];
+     std::sort(vec.begin(),vec.end());
+
+     for(int k=0; k<nfound;k++){
+       raw_tac_calibrator[j]->GetXaxis()->SetRange((vec.at(k)-100)/ps_per_chan,(vec.at(k)+100)/ps_per_chan);
+       vec[k]=raw_tac_calibrator[j]->GetMean();
+     }
+     
+     /*
+     for(int i=0; i<nfound; i++){
+       std::cout<<"TSpectrum point "<<i<<" has value : "<<spec.GetPositionX()[i] <<std::endl;//priting the peaks it has found to screen
+       std::cout<<"Vector point "<<i<<" has value : "<<vec.at(i) <<std::endl;//priting the peaks it has found to screen
+     }
+     */
 
      TGraph g;
-      for(int i=0;i<number_of_peaks;i++){
-		//time[i]= (double)(i)*calibrator_period;
-		//centroid[i]=(vec.at(i)+1)*ps_per_chan;
-		g.SetPoint(i,vec.at(i), (double)(i)*calibrator_period);
-		//calibration_hist[j]->SetBinContent((int)centroid[i],time[i]);
-		calibration_hist[j]->SetBinContent((int)vec.at(i)+1,(double)(i)*calibrator_period );
-	  }
-      TF1 * fitfunc = new TF1("fitfunc","[0] + [1]*x");
-      //TF1 * fitfunc = new TF1("fitfunc","[0] + [1]*x + [2]*x*x");
-      g.Fit(fitfunc,"Q");
-      calibration_hist[j]->Fit(fitfunc,"Q");
+     for(int i=0;i<nfound;i++){
+       if(vec.at(i)>10){
+	 g.SetPoint(i,vec.at(i), (double)(i)*calibrator_period);
+	 calibration_hist[j]->SetBinContent((int)vec.at(i)+1,(double)(i)*calibrator_period );
+       }
+     }
+     TF1 * fitfunc = new TF1("fitfunc","[0] + [1]*x");//linear polinomy
+     //TF1 * fitfunc = new TF1("fitfunc","[0] + [1]*x + [2]*x*x");//second order polinomy (it should not be necesary)
+     g.Fit(fitfunc,"Q");
+     calibration_hist[j]->Fit(fitfunc,"Q");
 
-      TGainMatch * gm = new TGainMatch();
-      gm->SetFitFunction(fitfunc);
-      gm->SetChannel(j+first_TAC_channel);//TAC channel, this may need a better implamentation
+     TGainMatch * gm = new TGainMatch();
+     gm->SetFitFunction(fitfunc);
+     gm->SetChannel(j+first_TAC_channel);//TAC channel, this may need a better implamentation
 
-      gm->GetFitFunction()->SetParameter(0,0);
-      gm->GetFitFunction()->SetParameter(1,(gm->GetFitFunction()->GetParameter(1)*ps_per_chan));   
+     gm->GetFitFunction()->SetParameter(0,0);
+     //gm->GetFitFunction()->SetParameter(1,(gm->GetFitFunction()->GetParameter(1)*ps_per_chan));   
 
-      gm->WriteToChannel();
-   
-      //if(gm) { delete gm; gm = NULL; }
-      //if(fitfunc) { delete fitfunc; fitfunc = NULL; }
+     gm->WriteToChannel();
 
+     vec.clear();//Clear the vector for the next iteration
    }
    
 	f->cd();
@@ -189,25 +177,36 @@ int main(int argc, char** argv) {
    std::cout << "--->>>    cal file written to:   " << calfile << std::endl;
 
 //Uncomment if you want to plot the calibrated TAC (right now it does a second loop on the AnalysisTree
+
 /*
+   TH1F * raw_tac[8]; 
+   for(int i=0; i<8; i++){
+     raw_tac[i] = new TH1F(Form("raw_TAC%d",i),Form("raw_TAC%d",i),(int) (tac_range/ps_per_chan),0,tac_range); list.Add(raw_tac[i]);
+   }
+
    TH1F * tac_calibrated[8];
    for(int i=0; i<8; i++){
-	tac_calibrated[i] = new TH1F(Form("tac_calibrated%d",i),Form("tac_calibrated%d",i),(tac_range/ps_per_chan),0,tac_range);list.Add(tac_calibrated[i]);
-	}
+     tac_calibrated[i] = new TH1F(Form("tac_calibrated%d",i),Form("tac_calibrated%d",i),(tac_range/ps_per_chan),0,tac_range);list.Add(tac_calibrated[i]);
+   }
 
    TH1F * tac_sum = new TH1F("tac_sum","tac_sum",(tac_range/ps_per_chan),0,tac_range);list.Add(tac_sum);
+
    for(int i=0; i<tree->GetEntries(); i++){
-      tree->GetEntry(i);
-		if(tac->GetMultiplicity()>0&&labr->GetMultiplicity()==0){
-			for(int j=0; j< tac->GetMultiplicity(); j++){
-			   	int histnum = tac->GetTACHit(j)->GetDetector()-1;
-			      if(histnum >= 0 && histnum <= 7){
-         			Float_t energy = tac->GetTACHit(j)->GetEnergy();
-			        tac_calibrated[histnum]->Fill(energy);
-         			tac_sum->Fill(energy);
-			}
-		}
-      }
+
+     tree->GetEntry(i);
+     
+     if(tac->GetMultiplicity()>0&&labr->GetMultiplicity()==0){
+       for(int j=0; j< tac->GetMultiplicity(); j++){
+	 int histnum = tac->GetTACHit(j)->GetDetector()-1;
+	 if(histnum >= 0 && histnum <= 7){
+	   Float_t energy = tac->GetTACHit(j)->GetEnergy();
+	   tac_calibrated[histnum]->Fill(energy);
+	   tac_sum->Fill(energy);
+	   double charge = tac->GetTACHit(j)->GetCharge();
+	   raw_tac[histnum]->Fill(charge);
+	 }
+       }
+     }
    }
 */
 
@@ -233,7 +232,7 @@ int main(int argc, char** argv) {
    }
 */
 
-
+//Only uncoment if you want an additional outfile with spectra to check the quality of your calibration
 /*
    std::string outname = argv[1];
    outname.replace(outname.begin(),outname.end()-14,"tac_calibration");
