ce853ae6af3b8426914e6887b85842c567e21ef0
VinzenzBildstein <vbildste@uoguelph.ca>
2019-12-04 11:00:59 -0500

    Fixed CrossTalk selector and TGRSIMap

--- include/TGRSIMap.h
+++ include/TGRSIMap.h
@@ -18,9 +18,6 @@
 /// This class re-implements std::map with more explicit
 /// expections replacing out-of-range exceptions.
 ///
-/// all constructors were copied from http://www.cplusplus.com/reference/map/map/map/ (c++11 tab)
-/// and from bits/stl_map.h of gcc 4.9.1
-///
 ////////////////////////////////////////////////////////////
 
 template <typename key_type>
@@ -28,41 +25,46 @@ class TGRSIMapException;
 
 template <typename key_type, typename mapped_type, typename key_compare = std::less<key_type>,
 			typename allocator_type = std::allocator<std::pair<const key_type, mapped_type> > >
-class TGRSIMap : public std::map<key_type, mapped_type, key_compare, allocator_type>
+class TGRSIMap
 {
 public:
-	TGRSIMap(const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type()) : std::map<key_type, mapped_type, key_compare, allocator_type>(comp, alloc) {}
-	TGRSIMap(const allocator_type& alloc) : std::map<key_type, mapped_type, key_compare, allocator_type>(alloc) {}
-	template <class InputIterator>
-		TGRSIMap(InputIterator first, InputIterator last,
-				const key_compare& comp = key_compare(),
-				const allocator_type& alloc = allocator_type()) : std::map<key_type, mapped_type, key_compare, allocator_type>(first, last, comp, alloc) {}
-	TGRSIMap(const TGRSIMap& x) : std::map<key_type, mapped_type, key_compare, allocator_type>(x) {}
-	TGRSIMap(const TGRSIMap& x, const allocator_type& alloc) : std::map<key_type, mapped_type, key_compare, allocator_type>(x, alloc) {}
-	TGRSIMap(TGRSIMap&& x) : std::map<key_type, mapped_type, key_compare, allocator_type>(x) {}
-	TGRSIMap(TGRSIMap&& x, const allocator_type& alloc) : std::map<key_type, mapped_type, key_compare, allocator_type>(x, alloc) {}
+	TGRSIMap() {}
 	~TGRSIMap() {}
 
 	void Print() {
-		for(auto it : *this) {
+		for(auto it : fMap) {
 			std::cout<<it.first<<" - "<<it.second<<std::endl;
 		}
 	}
 
 	mapped_type& at(const key_type& k) { 
 		try {
-			return std::map<key_type, mapped_type, key_compare, allocator_type>::at(k);
+			return fMap.at(k);
 		} catch(std::exception& e) {
-			throw TGRSIMapException<key_type>(k, *this);
+			throw TGRSIMapException<key_type>(k, fMap);
 		}
 	}
 	const mapped_type& at(const key_type& k) const {
 		try {
-			return std::map<key_type, mapped_type, key_compare, allocator_type>::at(k);
+			return fMap.at(k);
 		} catch(std::exception& e) {
-			throw TGRSIMapException<key_type>(k, *this);
+			throw TGRSIMapException<key_type>(k, fMap);
 		}
 	}
+
+	      mapped_type& operator[](const key_type& k)       { return at(k); }
+	const mapped_type& operator[](const key_type& k) const { return at(k); }
+
+	typedef std::map<key_type, mapped_type, key_compare, allocator_type> map_t;
+	typename map_t::iterator begin()       { return fMap.begin(); }
+	typename map_t::const_iterator begin() const { return fMap.begin(); }
+	typename map_t::iterator end()       { return fMap.end(); }
+	typename map_t::const_iterator end() const { return fMap.end(); }
+
+	typename map_t::size_type count(const key_type* k) const { return fMap.count(); }
+
+private:
+	std::map<key_type, mapped_type, key_compare, allocator_type> fMap;
 };
 
 template <typename key_type>
@@ -70,7 +72,7 @@ class TGRSIMapException : public std::exception
 {
 public:
 	template<typename mapped_type, typename key_compare, typename allocator_type>
-	TGRSIMapException(const key_type key, const TGRSIMap<key_type, mapped_type, key_compare, allocator_type>& map)
+	TGRSIMapException(const key_type key, const std::map<key_type, mapped_type, key_compare, allocator_type>& map)
 		: std::exception(), fKey(key)
 		{
 			for(auto it : map) {
