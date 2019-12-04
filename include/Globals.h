a0da2a262eb1a0a4affbdb86ac8c94d173f6b488
VinzenzBildstein <vbildste@uoguelph.ca>
2019-12-04 11:01:59 -0500

    Added global function PrintStacktrace

--- include/Globals.h
+++ include/Globals.h
@@ -60,10 +60,13 @@ typedef char int8_t;
 #include <cstdint>
 #endif
 
+#include <iostream>
 #include <stdexcept>
 #include <string>
 #include <cstdio>
 #include <cstdlib>
+#include <execinfo.h>
+#include <cxxabi.h>
 //#include <stdint.h>
 const std::string& ProgramName();
 
@@ -81,4 +84,78 @@ public:
    const char* message;
 };
 }
+
+// print a demangled stack backtrace of the caller function (copied from https://panthema.net/2008/0901-stacktrace-demangled/)
+static inline void PrintStacktrace(std::ostream& out = std::cout, unsigned int maxFrames = 63)
+{
+	out<<"stack trace:"<<std::endl;
+
+	// storage array for stack trace address data
+	void** addrlist = new void*[maxFrames+1];
+
+	// retrieve current stack addresses
+	int addrlen = backtrace(addrlist, maxFrames+1);
+
+	if(addrlen == 0) {
+		out<<"  <empty, possibly corrupt>"<<std::endl;
+		return;
+	}
+
+	// resolve addresses into strings containing "filename(function+address)",
+	// this array must be free()-ed
+	char** symbollist = backtrace_symbols(addrlist, addrlen);
+
+	// allocate string which will be filled with the demangled function name
+	size_t funcnamesize = 256;
+	char* funcname = new char[funcnamesize];
+
+	// iterate over the returned symbol lines. skip the first, it is the
+	// address of this function.
+	for(int i = 2; i < addrlen; i++) {
+		char* begin_name = nullptr;
+		char* begin_offset = nullptr;
+		char* end_offset = nullptr;
+
+		// find parentheses and +address offset surrounding the mangled name:
+		// ./module(function+0x15c) [0x8048a6d]
+		for(char* p = symbollist[i]; *p; ++p) {
+			if(*p == '(') {
+				begin_name = p;
+			} else if (*p == '+') {
+				begin_offset = p;
+			} else if(*p == ')' && begin_offset) {
+				end_offset = p;
+				break;
+			}
+		}
+
+		if(begin_name && begin_offset && end_offset && begin_name < begin_offset) {
+			*begin_name++ = '\0';
+			*begin_offset++ = '\0';
+			*end_offset = '\0';
+
+			// mangled name is now in [begin_name, begin_offset) and caller
+			// offset in [begin_offset, end_offset). now apply
+			// __cxa_demangle():
+
+			int status;
+			char* ret = abi::__cxa_demangle(begin_name, funcname, &funcnamesize, &status);
+			if(status == 0) {
+				funcname = ret; // use possibly realloc()-ed string
+				std::cout<<"  "<<symbollist[i]<<" : "<<funcname<<"+"<<begin_offset<<std::endl;
+			} else {
+				// demangling failed. Output function name as a C function with
+				// no arguments.
+				std::cout<<"  "<<symbollist[i]<<" : "<<begin_name<<"()+"<<begin_offset<<std::endl;
+			}
+		} else {
+			// couldn't parse the line? print the whole line.
+			std::cout<<"  "<<symbollist[i]<<std::endl;
+		}
+	}
+
+	free(funcname);
+	free(symbollist);
+}
+
 #endif
