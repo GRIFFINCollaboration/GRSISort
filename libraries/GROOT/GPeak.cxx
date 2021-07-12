150906b0d390fcc3e437f12281a665a3c703ddae
VinzenzBildstein <vbildste@uoguelph.ca>
2021-07-12 16:40:34 -0400

    Added conditions so that "no-print" actually prints nothing

--- libraries/GROOT/GPeak.cxx
+++ libraries/GROOT/GPeak.cxx
@@ -284,60 +284,23 @@ Bool_t GPeak::Fit(TH1* fithist, Option_t* opt)
 
    TFitResultPtr fitres = fithist->Fit(this, Form("%sLRSM", options.Data()));
 
-   // fitres.Get()->Print();
-   printf("chi^2/NDF = %.02f\n", GetChisquare() / static_cast<double>(GetNDF()));
+   if(!noprint) printf("chi^2/NDF = %.02f\n", GetChisquare() / static_cast<double>(GetNDF()));
 
    if(!fitres.Get()->IsValid()) {
-      printf(RED "fit has failed, trying refit... " RESET_COLOR);
-      // SetParameter(3,0.1);
-      // SetParameter(4,0.01);
-      // SetParameter(5,0.0);
+      if(verbose) printf(RED "fit has failed, trying refit... " RESET_COLOR);
       fithist->GetListOfFunctions()->Last()->Delete();
       fitres = fithist->Fit(this, Form("%sLRSME", options.Data())); //,Form("%sRSM",options.Data()))
-      if(fitres.Get()->IsValid()) {
-         printf(DGREEN " refit passed!" RESET_COLOR "\n");
-      } else {
-         printf(DRED " refit also failed :( " RESET_COLOR "\n");
-      }
+		if(verbose) {
+			if(fitres.Get()->IsValid()) {
+				printf(DGREEN " refit passed!" RESET_COLOR "\n");
+			} else {
+				printf(DRED " refit also failed :( " RESET_COLOR "\n");
+			}
+		}
    }
 
-   // if(fitres->ParError(2) != fitres->ParError(2)) { // checks if nan.
-   //  if(fitres->Parameter(3)<1) {
-   //    FixParameter(4,0);
-   //    FixParameter(3,1);
-   // printf("Beta may have broken the fit, retrying with R=0);
-   //    fithist->GetListOfFunctions()->Last()->Delete();
-   //    fitres = fithist->Fit(this,Form("%sRSM",options.Data()));
-   //  }
-   //}
-
-   // TF1::Print();
-
-   // Double_t binwidth = fithist->GetBinWidth(GetParameter("centroid"));
-   // Double_t width    = TF1::GetParameter("sigma");
    Double_t xlow, xhigh;
-   // Double_t int_low,int_high;
    TF1::GetRange(xlow, xhigh);
-   // int_low  = xlow - 5.*width;
-   // int_high = xhigh +5.*width;
-
-   // Make a function that does not include the background
-   // Intgrate the background.
-   // GPeak *tmppeak = new GPeak;
-   // Copy(*tmppeak);
-
-   // tmppeak->SetParameter("bg_offset",0.0);
-   // tmppeak->SetRange(int_low,int_high);//This will help get the true area of the gaussian 200 ~ infinity in a gaus
-   // tmppeak->SetName("tmppeak");
-
-   // fArea = (tmppeak->Integral(int_low,int_high))/binwidth;
-   //  TMatrixDSym CovMat = fitres->GetCovarianceMatrix();
-   //  CovMat(6,6) = 0.0;
-   //  CovMat(7,7) = 0.0;
-   //  CovMat(8,8) = 0.0;
-   //  CovMat(9,9) = 0.0;
-
-   //  fDArea = (tmppeak->IntegralError(int_low,int_high,tmppeak->GetParameters(),CovMat.GetMatrixArray()))/binwidth;
 
    double bgpars[5];
    bgpars[0] = TF1::GetParameters()[0];
@@ -345,10 +308,8 @@ Bool_t GPeak::Fit(TH1* fithist, Option_t* opt)
    bgpars[2] = TF1::GetParameters()[2];
    bgpars[3] = TF1::GetParameters()[5];
    bgpars[4] = TF1::GetParameters()[6];
-   // bgpars[5] = TF1::GetParameters()[7];
 
    fBGFit.SetParameters(bgpars);
-   // fithist->GetListOfFunctions()->Print();
 
    fArea         = Integral(xlow, xhigh) / fithist->GetBinWidth(1);
    double bgArea = fBGFit.Integral(xlow, xhigh) / fithist->GetBinWidth(1);
@@ -359,10 +320,10 @@ Bool_t GPeak::Fit(TH1* fithist, Option_t* opt)
    }
    fSum = fithist->Integral(fithist->GetXaxis()->FindBin(xlow),
                             fithist->GetXaxis()->FindBin(xhigh)); //* fithist->GetBinWidth(1);
-   printf("sum between markers: %02f\n", fSum);
+   if(!noprint) printf("sum between markers: %02f\n", fSum);
    fDSum = TMath::Sqrt(fSum);
    fSum -= bgArea;
-   printf("sum after subtraction: %02f\n", fSum);
+   if(!noprint) printf("sum after subtraction: %02f\n", fSum);
 
    // Make a function that does not include the background
    // Intgrate the background.
@@ -389,28 +350,16 @@ Bool_t GPeak::Fit(TH1* fithist, Option_t* opt)
 
    delete tmppeak;
 
-
-
-   if(!verbose) {
-      Print(); /*
-       printf("BG Area:         %.02f\n",bgArea);
-       printf("GetChisquared(): %.4f\n", TF1::GetChisquare());
-       printf("GetNDF():        %i\n",   TF1::GetNDF());
-       printf("GetProb():       %.4f\n", TF1::GetProb());*/
-      // TF1::Print();
+   if(verbose) {
+      Print();
    }
 
-   // printf("fithist->GetListOfFunctions()->FindObject(this) =
-   // 0x%08x\n",fithist->GetListOfFunctions()->FindObject(GetName()));
-   // fflush(stdout);
    Copy(*fithist->GetListOfFunctions()->FindObject(GetName()));
-   //  fithist->GetListOfFunctions()->Add(&fBGFit); //use to be a clone.
    fithist->GetListOfFunctions()->Add(fBGFit.Clone()); // use to be a clone.
    fLastFit = this;
 
-   SetParent(nullptr); // fithist);
+   SetParent(nullptr);
 
-   // delete tmppeak;
    return true;
 }
 
