77378af500c6bb327be36a3b55e7ae79286c979e
VinzenzBildstein <vbildste@uoguelph.ca>
2023-10-25 11:13:11 -0400

    Added option for subdirectories for analysis and fragment trees and log files to sort script

--- examples/sort.sh
+++ examples/sort.sh
@@ -3,6 +3,9 @@
 DATADIR=<your data directory here>
 SORTOPTIONS="your grsisort options here, e.g. --recommended"
 CALFILES="any and all cal-files you want to use"
+ANALYSISTREES="." # directory for analysis trees
+FRAGMENTTREES="." # directory for fragment trees
+LOGFILES="." # directory for log files
 
 firstRun=1
 lastRun=1
@@ -33,13 +36,13 @@ for run in `seq $firstRun $lastRun` ; do
    for midasFile in `find ${DATADIR} -maxdepth 1 -amin +3 -name "run${run}_???.mid"` ; do
       subrun=$(basename $midasFile | cut -d '_' -f2 | cut -d '.' -f1)
 		# if the analysis file exists, we don't re-run the analysis
-      if [ -e analysis${run}_${subrun}.root ] ; then
+      if [ -e ${ANALYSISTREES}/analysis${run}_${subrun}.root ] ; then
          continue
       fi
 		# touching the file means it exists now and if we run this script in multiple terminal
 		# the other scripts won't try and start sorting this file
-      touch analysis${run}_${subrun}.root
-      logFile="log${run}_${subrun}.txt"
+      touch ${ANALYSISTREES}/analysis${run}_${subrun}.root
+      logFile="${LOGFILES}/log${run}_${subrun}.txt"
 		# print the sort command to the log-file
       echo "grsisort $SORTOPTIONS $CALFILES $midasFile" | tee $logFile
 		# start sorting and append all output to the log-file
@@ -47,6 +50,9 @@ for run in `seq $firstRun $lastRun` ; do
       grsisort $SORTOPTIONS $CALFILES $midasFile 2>&1 | tee -a $logFile
 		# if necessary you can make certain file-permissions are set correctly
 		# e.g. when sorting the same data for multiple people
-      chmod 664 analysis${run}_${subrun}.root fragment${run}_${subrun}.root log${run}_${subrun}.txt
+      chmod 664 ${ANALYSISTREES}/analysis${run}_${subrun}.root fragment${run}_${subrun}.root log${run}_${subrun}.txt
+		# moved the output files into the right directories, or if that fails, remove the file we touched at the beginning
+		mv analysis${run}_${subrun}.root ${ANALYSISTREES}/analysis${run}_${subrun}.root || rm ${ANALYSISTREES}/analysis${run}_${subrun}.root
+      mv fragment${run}_${subrun}.root ${FRAGMENTTREES}/fragment${run}_${subrun}.root || rm ${ANALYSISTREES}/analysis${run}_${subrun}.root
    done
 done
