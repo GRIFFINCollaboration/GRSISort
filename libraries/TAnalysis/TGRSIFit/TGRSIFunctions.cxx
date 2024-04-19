669aaacc691814498818db3ccb9189bc3f43dda0
VinzenzBildstein <vbildste@uoguelph.ca>
2024-04-19 12:56:39 -0400

    Not using wigner_6j function if MathMore isn't available

--- libraries/TAnalysis/TGRSIFit/TGRSIFunctions.cxx
+++ libraries/TAnalysis/TGRSIFit/TGRSIFunctions.cxx
@@ -496,7 +496,12 @@ double TGRSIFunctions::ClebschGordan(double j1, double m1, double j2, double m2,
 
 double TGRSIFunctions::RacahW(double a, double b, double c, double d, double e, double f)
 {
+#ifdef HAS_MATHMORE
 	return TMath::Power((-1), int(a+b+d+c))*ROOT::Math::wigner_6j(int(2*a),int(2*b),int(2*e),int(2*d),int(2*c),int(2*f));
+#else
+	std::cout<<"Mathmore feature of ROOT is missing, "<<__PRETTY_FUNCTION__<<" will always return 1!"<<std::endl;
+	return 1.;
+#endif
 }
 
 double TGRSIFunctions::F(double k, double jf, double L1, double L2, double ji)
