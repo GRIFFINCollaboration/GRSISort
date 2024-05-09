536642c84c14339b377aaf647a91780d401cad54
VinzenzBildstein <vbildste@uoguelph.ca>
2024-05-09 14:59:19 -0400

    Added warning if trying to use LeanAnalyzeComptonMatrices w/o mathmore

--- util/LeanAnalyzeComptonMatrices.cxx
+++ util/LeanAnalyzeComptonMatrices.cxx
@@ -87,7 +87,7 @@ double PolarizationCalculation();
 TList* AGATATheory(TList* list, double Q);
 double ScaleQ(double E1, double E2);
 
-
+#ifdef HAS_MATHMORE
 //Main
 int main(int argc, char **argv)
 {
@@ -128,6 +128,13 @@ int main(int argc, char **argv)
   
   return 0;
 }
+#else
+int main(int, char** argv)
+{
+	std::cout<<"No mathmore support in ROOT, "<<argv[0]<<" can't run!"<<std::endl;
+	return 1;
+}
+#endif
 
 
 // ****************************************************************** //
