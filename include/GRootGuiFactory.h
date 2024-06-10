733ded7ecef0e9285e447029a5d8691d72772b24
VinzenzBildstein <vbildste@uoguelph.ca>
2024-06-10 14:11:30 -0400

    Changed comment to doxygen format

--- include/GRootGuiFactory.h
+++ include/GRootGuiFactory.h
@@ -2,16 +2,16 @@
 #ifndef ROOT_GRootGuiFactory
 #define ROOT_GRootGuiFactory
 
-//////////////////////////////////////////////////////////////////////////
-//                                                                      //
-// GRootGuiFactory                                                      //
-//                                                                      //
-// This class is a factory for ROOT GUI components. It overrides        //
-// the member functions of the ABS TGuiFactory.  This is necessary      //
-// in order to override the native root canvas and fully take control   //
-// of the users interface with objects drawn to the screen   pcb.       //
-//                                                                      //
-//////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+///
+/// \class GRootGuiFactory
+///
+/// This class is a factory for ROOT GUI components. It overrides
+/// the member functions of the ABS TGuiFactory.  This is necessary
+/// in order to override the native root canvas and fully take control
+/// of the users interface with objects drawn to the screen   pcb.
+///
+///////////////////////////////////////////////////////////////////////////
 
 #ifndef ROOT_TGuiFactory
 #include "TGuiFactory.h"
