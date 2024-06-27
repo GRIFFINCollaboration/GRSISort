bacc6dbb7a302102eb56376c4dc23ce6f4e7d0ce
VinzenzBildstein <vbildste@uoguelph.ca>
2024-06-27 17:01:26 -0400

    Changes from clang-tidy

--- include/TEpicsFrag.h
+++ include/TEpicsFrag.h
@@ -30,31 +30,24 @@
 
 class TEpicsFrag : public TObject {
 public:
-   TEpicsFrag();
-   ~TEpicsFrag() override;
+   TEpicsFrag() = default;
+   ~TEpicsFrag() override = default;
 
-   time_t fDaqTimeStamp;   //->  Timestamp of the daq event
-   Int_t  fDaqId;          //->  daq ID
-
-   std::vector<float>       fData;   ///< The data in the scaler
-   std::vector<std::string> fName;   ///< The name of the scaler
-
-   int          GetSize() { return fData.size(); }
-   inline float GetData(const unsigned int& i)
+   size_t       GetSize() const { return fData.size(); }
+   inline float GetData(const unsigned int& index) const
    {
-      if(i >= fData.size()) {
+      if(index >= fData.size()) {
          return fData.back();
-      } else {
-         return fData[i];
       }
+		return fData[index];
    }
 
    void Clear(Option_t* opt = "") override;         //!<!
    void Print(Option_t* opt = "") const override;   //!<!
 
    static void        AddEpicsVariable(const char* name);
-   static void        SetEpicsNameList(const std::vector<std::string>& name_vec);
-   static std::string GetEpicsVariableName(const int& i);
+   static void        SetEpicsNameList(const std::vector<std::string>& names);
+   static std::string GetEpicsVariableName(const int& index);
    static void        PrintVariableNames();
 
    static void BuildScalerMap(TTree* tree);
@@ -67,6 +60,12 @@ private:
    static std::vector<std::string>       fNameList;   // This stuff should potentially move to a run info of some sort
    static std::map<Long64_t, TEpicsFrag> fScalerMap;
    static Long64_t                       fSmallestTime;
+
+   time_t fDaqTimeStamp{0};   //->  Timestamp of the daq event
+   Int_t  fDaqId{-1};         //->  daq ID
+
+   std::vector<float>       fData;   ///< The data in the scaler
+   std::vector<std::string> fName;   ///< The name of the scaler
 
    /// \cond CLASSIMP
    ClassDefOverride(TEpicsFrag, 2);   // Scaler Fragments
