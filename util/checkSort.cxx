1a9bfe46e7e3db7c9db3dcd60748024848584571
VinzenzBildstein <vbildste@uoguelph.ca>
2024-07-04 15:11:17 -0400

    Changes from clang-tidy

--- util/checkSort.cxx
+++ util/checkSort.cxx
@@ -23,37 +23,37 @@ int main(int argc, char** argv)
          return 1;
       }
 
-      TTree* tree = static_cast<TTree*>(input.Get("AnalysisTree"));
+      auto* tree = static_cast<TTree*>(input.Get("AnalysisTree"));
       if(tree == nullptr) {
          std::cout << "No AnalysysTree in " << argv[file] << std::endl;
          continue;
       }
 
       // loop over all branches and add the detector to the list and set up the list of last time stamps and energies
-      auto                                   branches = tree->GetListOfBranches();
+      auto*                                  branches = tree->GetListOfBranches();
       std::vector<TDetector*>                det(branches->GetEntries());
       std::map<TClass*, std::deque<int64_t>> lastTS;
       std::map<TClass*, std::deque<double>>  lastEn;
 
-      size_t d = 0;
+      size_t index = 0;
       for(auto branch : *(branches)) {
-         auto cl = TClass::GetClass(branch->GetName());
-         det[d]  = static_cast<TDetector*>(cl->New());
-         tree->SetBranchAddress(branch->GetName(), &det[d]);
-         lastTS[cl] = std::deque<int64_t>(3, 0);
-         lastEn[cl] = std::deque<double>(3, 0.);
-         ++d;
+         auto cls = TClass::GetClass(branch->GetName());
+         det[index]  = static_cast<TDetector*>(cls->New());
+         tree->SetBranchAddress(branch->GetName(), &det[index]);
+         lastTS[cls] = std::deque<int64_t>(3, 0);
+         lastEn[cls] = std::deque<double>(3, 0.);
+         ++index;
       }
 
       Long64_t nEntries = tree->GetEntries();
-      Long64_t e        = 0;
+      Long64_t entry        = 0;
 
-      for(e = 0; e < nEntries; ++e) {
+      for(entry = 0; entry < nEntries; ++entry) {
          // check if we can advance 100 events, otherwise read last event
-         if(e + 100 < nEntries) e += 100;
-         else e = nEntries - 1;
+         if(entry + 100 < nEntries) { entry += 100; }
+         else                       { entry = nEntries - 1; }
 
-         tree->GetEntry(e);
+         tree->GetEntry(entry);
 
          if(e % 1000 == 0) {
             std::cout << "\r" << (100 * e) / nEntries << "% done" << std::flush;
@@ -62,36 +62,36 @@ int main(int argc, char** argv)
          // re-using the d-variable from above
          for(d = 0; d < det.size(); ++d) {
             // skip empty detectors
-            if(det[d]->GetMultiplicity() < 1) continue;
-            auto          cl  = det[d]->IsA();
+            if(det[d]->GetMultiplicity() < 1) { continue; }
+            auto*         cls = det[d]->IsA();
             TDetectorHit* hit = det[d]->GetHit(0);
             if(hit != nullptr) {
                bool sameE = true;
-               for(auto energy : lastEn[cl]) {
+               for(auto energy : lastEn[cls]) {
                   if(hit->GetEnergy() != energy) {
                      sameE = false;
                      break;
                   }
                }
                bool sameTS = true;
-               for(auto ts : lastTS[cl]) {
-                  if(hit->GetTimeStamp() != ts) {
+               for(auto timestamp : lastTS[cls]) {
+                  if(hit->GetTimeStamp() != timestamp) {
                      sameTS = false;
                      break;
                   }
                }
                if(sameE && sameTS) {
-                  std::cerr << "\r" << RED << "Got the same timestamp and energy for entry " << e << ". File " << YELLOW << argv[file] << RED << " is bad!" << RESET_COLOR << std::endl;
+                  std::cerr << "\r" << RED << "Got the same timestamp and energy for entry " << entry << ". File " << YELLOW << argv[file] << RED << " is bad!" << RESET_COLOR << std::endl;
                   break;
                }
-               lastTS[cl].emplace_back(hit->GetTimeStamp());
-               lastEn[cl].emplace_back(hit->GetEnergy());
-               lastTS[cl].pop_front();
-               lastEn[cl].pop_front();
+               lastTS[cls].emplace_back(hit->GetTimeStamp());
+               lastEn[cls].emplace_back(hit->GetEnergy());
+               lastTS[cls].pop_front();
+               lastEn[cls].pop_front();
             }
          }
          // if we broke out of the previous loop, break out of this one as well
-         if(d != det.size()) break;
+         if(d != det.size()) { break; }
       }   // loop over entries
       if(e == nEntries) {
          std::cerr << "\r" << GREEN << "File " << BLUE << argv[file] << GREEN << " is good!" << RESET_COLOR << std::endl;
