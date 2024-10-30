bebb66a0e24eb5116e445be80b7c4a56da1f4fba
VinzenzBildstein <7277216+VinzenzBildstein@users.noreply.github.com>
2024-10-30 16:40:27 +0000

    [MegaLinter] Apply linters automatic fixes

--- include/TRedirect.h
+++ include/TRedirect.h
@@ -26,18 +26,18 @@
 class TRedirect {
 public:
    TRedirect(const char* newOut, const char* newErr, bool append = true)
-		: fOutFile(newOut), fErrFile(newErr)
+      : fOutFile(newOut), fErrFile(newErr)
    {
       Redirect(append);
    }
    explicit TRedirect(const char* newOut, bool append = true)
-		: fOutFile(newOut), fErrFile(newOut)
+      : fOutFile(newOut), fErrFile(newOut)
    {
       Redirect(append);
    }
 
-	const char* OutFile() { return fOutFile; }
-	const char* ErrFile() { return fErrFile; }
+   const char* OutFile() { return fOutFile; }
+   const char* ErrFile() { return fErrFile; }
 
    ~TRedirect()
    {
@@ -56,26 +56,26 @@ public:
 private:
    void Redirect(bool append)
    {
-		if(fOutFile != nullptr) {
-			fStdOutFileDescriptor = dup(fileno(stdout));
-			fflush(stdout);
-			int newStdOut = open(fOutFile, (append ? O_WRONLY | O_CREAT | O_APPEND : O_WRONLY | O_CREAT), S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-			dup2(newStdOut, fileno(stdout));
-			close(newStdOut);
-		}
-		if(fErrFile != nullptr) {
-			fStdErrFileDescriptor = dup(fileno(stderr));
-			fflush(stderr);
-			int newStdErr = open(fErrFile, (append ? O_WRONLY | O_CREAT | O_APPEND : O_WRONLY | O_CREAT), S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-			dup2(newStdErr, fileno(stderr));
-			close(newStdErr);
-		}
-	}
+      if(fOutFile != nullptr) {
+         fStdOutFileDescriptor = dup(fileno(stdout));
+         fflush(stdout);
+         int newStdOut = open(fOutFile, (append ? O_WRONLY | O_CREAT | O_APPEND : O_WRONLY | O_CREAT), S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+         dup2(newStdOut, fileno(stdout));
+         close(newStdOut);
+      }
+      if(fErrFile != nullptr) {
+         fStdErrFileDescriptor = dup(fileno(stderr));
+         fflush(stderr);
+         int newStdErr = open(fErrFile, (append ? O_WRONLY | O_CREAT | O_APPEND : O_WRONLY | O_CREAT), S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+         dup2(newStdErr, fileno(stderr));
+         close(newStdErr);
+      }
+   }
 
-   int fStdOutFileDescriptor{0};
-   int fStdErrFileDescriptor{0};
-	const char* fOutFile{nullptr};
-	const char* fErrFile{nullptr};
+   int         fStdOutFileDescriptor{0};
+   int         fStdErrFileDescriptor{0};
+   const char* fOutFile{nullptr};
+   const char* fErrFile{nullptr};
 };
 
 #endif
