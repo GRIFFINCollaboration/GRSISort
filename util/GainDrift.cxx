3ef987bf7aa844149b3e50cf31a046f2cfc5f565
VinzenzBildstein <vbildste@uoguelph.ca>
2025-07-11 12:04:12 -0400

    Added min. FWHM for good peaks and user selected degree of polynomial

--- util/GainDrift.cxx
+++ util/GainDrift.cxx
@@ -15,18 +15,20 @@
 #include "TPeakFitter.h"
 #include "TRWPeak.h"
 
-void FindGainDrift(TH2* hist, std::vector<double> energies, std::vector<double> energyUncertainties, double range, const std::string& prefix, const std::string& label, TFile* output);
+void FindGainDrift(TH2* hist, std::vector<double> energies, std::vector<double> energyUncertainties, double range, double minFWHM, int degree, const std::string& prefix, const std::string& label, TFile* output);
 
 int main(int argc, char** argv)
 {
    bool printUsage = (argc == 1);
 
+	int							 degree = 1;
    double                   range = 10.;
    std::vector<double>      energies;
    std::vector<double>      energyUncertainties;
    std::vector<std::string> fileNames;
    std::string              prefix        = "GainDrift";
    std::string              histogramName = "EnergyVsChannel";
+   double                   minFWHM       = 2.;
 
    for(int i = 1; i < argc; ++i) {
       if(strcmp(argv[i], "-if") == 0) {
@@ -41,7 +43,9 @@ int main(int argc, char** argv)
       } else if(strcmp(argv[i], "-sf") == 0) {
          if(i + 1 < argc) {
             TUserSettings settings(argv[++i]);
+            degree = settings.GetInt("Degree", 1);
             range = settings.GetDouble("Range", 10.);
+            minFWHM = settings.GetDouble("MinimumFWHM", 2.);
             prefix = settings.GetString("Prefix", "GainDrift");
             histogramName = settings.GetString("HistogramName", "EnergyVsChannel");
             if(!energies.empty()) {
@@ -92,11 +96,25 @@ int main(int argc, char** argv)
             // if we get here we can add the next argument to the list of energies
             energyUncertainties.push_back(std::atof(argv[++i]));
          }
+      } else if(strcmp(argv[i], "-d") == 0) {
+         if(i + 1 < argc) {
+            degree = std::atoi(argv[++i]);
+         } else {
+            std::cout << "Error, -d flag needs an argument!" << std::endl;
+            printUsage = true;
+         }
       } else if(strcmp(argv[i], "-r") == 0) {
          if(i + 1 < argc) {
             range = std::atof(argv[++i]);
          } else {
             std::cout << "Error, -r flag needs an argument!" << std::endl;
+            printUsage = true;
+         }
+      } else if(strcmp(argv[i], "-mw") == 0) {
+         if(i + 1 < argc) {
+            minFWHM = std::atof(argv[++i]);
+         } else {
+            std::cout << "Error, -mw flag needs an argument!" << std::endl;
             printUsage = true;
          }
       } else if(strcmp(argv[i], "-hn") == 0) {
@@ -113,7 +131,10 @@ int main(int argc, char** argv)
             std::cout << "Error, -pre flag needs an argument!" << std::endl;
             printUsage = true;
          }
-      }
+      } else {
+         std::cerr << "Error, unknown flag \"" << argv[i] << "\"!" << std::endl;
+         printUsage = true;
+      }  
    }
 
    // check that we got the necessary parameters set
@@ -128,16 +149,19 @@ int main(int argc, char** argv)
 
 	// check that the number of uncertainties matches the number of energies
 	if(energyUncertainties.size() != energies.size()) {
+		std::cerr << "Error, mismatch between number of energies (" << energies.size() << ") and number of energy uncertainties (" << energyUncertainties.size() << ")!" << std::endl;
       printUsage = true;
    }
 
    if(printUsage) {
-      std::cout << "Arguments for " << argv[0] << ":" << std::endl
+      std::cerr << "Arguments for " << argv[0] << ":" << std::endl
                 << "-if  <input files>                                (required)" << std::endl
                 << "-sf  <settings file>                              (semi-optional, TUserSettings format identifying the peaks to be used)" << std::endl
                 << "-el  <list of energies to be used>                (semi-optional, either this or a settings file need to be used)" << std::endl
                 << "-ul  <list of energy uncertainties to be used>    (optional, default = empty)" << std::endl
+                << "-d   <degree of fit>                              (optional, default = 1, i.e. linear)" << std::endl
                 << "-r   <+-range of fit>                             (optional, default = 10.)" << std::endl
+                << "-mw  <minimum FWHM for good fit>                  (optional, default = 2.)" << std::endl
                 << "-hn  <histogram name>                             (optional, default = \"EnergyVsChannel\")" << std::endl
                 << "-pre <prefix>                                     (optional, default = \"GainDrift\")" << std::endl;
 
@@ -172,7 +196,7 @@ int main(int argc, char** argv)
       TRunInfo::ReadInfoFromFile(file);
       auto label = TRunInfo::CreateLabel(true);
       // find the gain drift for all channels
-      FindGainDrift(hist, energies, energyUncertainties, range, prefix, label, output);
+      FindGainDrift(hist, energies, energyUncertainties, range, minFWHM, degree, prefix, label, output);
 
       // write the gain drift to new cal-file
       TChannel::WriteCalFile(Form("%s%s.cal", prefix.c_str(), label.c_str()));
@@ -189,12 +213,12 @@ int main(int argc, char** argv)
    return 0;
 }
 
-bool GoodFit(TRWPeak& peak, const double& low, const double& high)
+bool GoodFit(TRWPeak& peak, const double& low, const double& high, double& minFWHM)
 {
    // for a good fit the centroid should be in the fitting range
    if(peak.Centroid() < low || high < peak.Centroid()) { return false; }
-   // for a good fit the FWHM should be less than half the fitting range (half is kind of arbitrary)
-   if(peak.FWHM() > (high - low) / 2.) { return false; }
+   // for a good fit the FWHM should be less than half the fitting range (half is kind of arbitrary), and larger than the minimum (2 is default value)
+   if(peak.FWHM() < minFWHM || peak.FWHM() > (high - low) / 2.) { return false; }
    // for a good fit the area should be positive (can't really put a non-arbitrary positive limit on this)
    if(peak.Area() < 0.) { return false; }
 
@@ -202,10 +226,19 @@ bool GoodFit(TRWPeak& peak, const double& low, const double& high)
    return true;
 }
 
-void FindGainDrift(TH2* hist, std::vector<double> energies, std::vector<double> energyUncertainties, double range, const std::string& prefix, const std::string& label, TFile* output)
+double Polynomial(double* x, double* par) {
+	double result = par[1];
+	for(int i = 0; i < par[0]; ++i) {
+		result += par[i+2] * TMath::Power(x[0], i+1);
+	}
+	return result;
+}
+
+void FindGainDrift(TH2* hist, std::vector<double> energies, std::vector<double> energyUncertainties, double range, double minFWHM, int degree, const std::string& prefix, const std::string& label, TFile* output)
 {
    auto* yAxis  = hist->GetYaxis();
-   TF1*  linear = new TF1("linear", "[0] + [1]*x", yAxis->GetBinLowEdge(yAxis->GetFirst()), yAxis->GetBinLowEdge(yAxis->GetLast()));
+   TF1*  polynomial = new TF1("polynomial", Polynomial, yAxis->GetBinLowEdge(yAxis->GetFirst()), yAxis->GetBinLowEdge(yAxis->GetLast()), degree+2);
+	polynomial->FixParameter(0, degree);
 
    // loop over all x-bins
    for(int bin = 1; bin <= hist->GetXaxis()->GetNbins(); ++bin) {
@@ -218,6 +251,7 @@ void FindGainDrift(TH2* hist, std::vector<double> energies, std::vector<double> 
       auto* redirect = new TRedirect("/dev/null", true);
       auto* graph    = new TGraphErrors;
       graph->SetName(Form("graph%s_%d", label.c_str(), bin));
+		graph->SetTitle(Form("Channel %s, bin %d;uncorr. energy;nominal energy", label.c_str(), bin));
 		int graphIndex = 0;
       for(size_t i = 0; i < energies.size(); ++i) {
          TPeakFitter pf(energies[i] - range, energies[i] + range);
@@ -227,9 +261,9 @@ void FindGainDrift(TH2* hist, std::vector<double> energies, std::vector<double> 
 
          proj->GetListOfFunctions()->Add(pf.GetFitFunction()->Clone(Form("fit%.1f", energies[i])));
          // check if the fit was good
-         if(GoodFit(peak, energies[i] - range, energies[i] + range)) {
-            graph->SetPoint(graphIndex, energies[i], peak.Centroid());
-            graph->SetPointError(graphIndex, energyUncertainties[i], peak.CentroidErr());
+         if(GoodFit(peak, energies[i] - range, energies[i] + range, minFWHM)) {
+            graph->SetPoint(graphIndex, peak.Centroid(), energies[i]);
+            graph->SetPointError(graphIndex, peak.CentroidErr(), energyUncertainties[i]);
 				++graphIndex;
          } else {
             static_cast<TF1*>(proj->GetListOfFunctions()->Last())->SetLineColor(kGray);
@@ -237,16 +271,24 @@ void FindGainDrift(TH2* hist, std::vector<double> energies, std::vector<double> 
          }
       }
 
-      // reset the parameters of the linear function and fit the graph
-      linear->SetParameters(0., 1.);
-      graph->Fit(linear, "q");
+      // reset the parameters of the polynomial function and fit the graph
+      polynomial->SetParameter(1, 0.);
+      polynomial->SetParameter(2, 1.);
+		for(int i = 3; i < degree + 2; ++i) {
+			polynomial->SetParameter(i, 0.);
+		}
+      graph->Fit(polynomial, "q");
       delete redirect;
 
       redirect = new TRedirect(Form("%s.log", prefix.c_str()), true);
 
       std::cout << "bin " << bin << ":" << std::endl;
       graph->Print();
-      std::cout << "linear fit: " << linear->GetParameter(0) << " + " << linear->GetParameter(1) << " * x" << std::endl;
+      std::cout << "polynomial fit " << degree << ". degree: " << polynomial->GetParameter(1) << " + " << polynomial->GetParameter(2) << " * x";
+		for(int i = 3; i < degree + 2; ++i) {
+			std::cout << " + " << polynomial->GetParameter(i) << " * x^" << i-1;
+		}
+		std::cout << std::endl;
       std::cout << "========================================" << std::endl;
 
       // get the channel belonging to this bin
@@ -259,7 +301,10 @@ void FindGainDrift(TH2* hist, std::vector<double> energies, std::vector<double> 
       }
 
       // update the energy drift coefficents
-      std::vector<Float_t> coeff = {static_cast<Float_t>(linear->GetParameter(0)), static_cast<Float_t>(linear->GetParameter(1))};
+      std::vector<Float_t> coeff(degree+1);
+		for(int i = 0; i < degree+1; ++i) {
+			coeff[i] = static_cast<Float_t>(polynomial->GetParameter(i+1));
+		}
       TChannel::GetChannel(address)->SetENGDriftCoefficents(TPriorityValue<std::vector<Float_t>>(coeff, EPriority::kForce));
 
       // write spectrum and graph to output file
