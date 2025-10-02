028164ab62ef4876659ad863d110f8f270afd3b7
VinzenzBildstein <vbildste@uoguelph.ca>
2025-10-02 15:20:07 -0400

    Fixing bad bug in last commit

--- libraries/TFormat/TDetector.cxx
+++ libraries/TFormat/TDetector.cxx
@@ -27,20 +27,23 @@ void TDetector::Copy(TObject& rhs) const
    // to copy the hits without creating a memory leak we need to check
    // if the right-hand side has more hits than this 
    // if so, we need to delete the hits pointed to by the right-hand side
-   auto& hits = static_cast<TDetector&>(rhs).fHits;
-   if(hits.size() > fHits.size()) {
-      for(size_t i = fHits.size(); i < hits.size(); ++i) {
-         delete hits[i];
+   auto& rhsHits = static_cast<TDetector&>(rhs).fHits;
+   if(rhsHits.size() > fHits.size()) {
+      for(size_t i = fHits.size(); i < rhsHits.size(); ++i) {
+         delete rhsHits[i];
       }
-      hits.resize(fHits.size());
-   } else if(hits.size() < fHits.size()) {
-      // right-hand side has less hits, that means there is at least one we can use to determine the type
-      // we need to use IsA()->New() to make a new hit of whatever derived type this actually is
-      hits.resize(fHits.size(), static_cast<TDetectorHit*>(fHits[0]->IsA()->New()));
+      rhsHits.resize(fHits.size());
+   } else if(rhsHits.size() < fHits.size()) {
+      size_t oldSize = rhsHits.size();
+      rhsHits.resize(fHits.size(), nullptr);
+      for(size_t i = oldSize; i < rhsHits.size(); ++i) {
+         // we need to use IsA()->New() to make a new hit of whatever derived type this actually is
+         rhsHits[i] = static_cast<TDetectorHit*>(fHits[i]->IsA()->New());
+      }
    }
    // we have now ensured that the size of the two vectors is the same, so we can copy the contents of the hits
    for(size_t i = 0; i < fHits.size(); ++i) {
-      fHits[i]->Copy(*(hits[i]), true);
+      fHits[i]->Copy(*(rhsHits[i]), true);
    }
 }
 
